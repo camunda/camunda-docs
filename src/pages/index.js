36c29166cc195e648dede0ca63f3d3346a3db8e7
Adam <i@adamurban.de>
2020-09-18 19:37:01 +0200

    :tada:

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl('docs/guides/')}>
               Get Started
             </Link>
           </div>
