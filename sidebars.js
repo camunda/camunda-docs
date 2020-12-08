a292f0196da87f0157b7fd884b502bedcbcf5249
Adam <i@adamurban.de>
2020-12-08 14:02:19 +0100

    remove involve humans from gsg

--- sidebars.js
+++ sidebars.js
@@ -11,7 +11,7 @@ module.exports = {
         "guides/getting-started/deploy-your-process-and-start-process-instance",
         "guides/getting-started/implement-service-task",
         "guides/getting-started/implement-decision-gateway",
-        "guides/getting-started/involve-humans",
+        // "guides/getting-started/involve-humans",
         "guides/getting-started/monitor-your-process-in-operate",
       ],
     },
