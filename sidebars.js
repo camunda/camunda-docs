9ecfc784ee7ead1186d6c3af8f0a0030dafdd2f0
Amara <amara.graham@camunda.com>
2021-05-05 20:16:53 -0500

    split out annoucements and supported env

--- sidebars.js
+++ sidebars.js
@@ -324,6 +324,7 @@ module.exports = {
     "reference/release-policy",
     //"reference/deprecated-features",
     "reference/announcements",
+    "reference/supported-environments",
     "reference/licenses",
     "reference/notices",
   ]
