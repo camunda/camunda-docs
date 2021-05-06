f2074ef0eb37a73686b217cad39dcc691b11556c
Adam <i@adamurban.de>
2021-05-06 08:01:45 +0200

    fix(gtag): window undefined

--- src/theme/Footer/gtag.js
+++ src/theme/Footer/gtag.js
@@ -1,10 +1,12 @@
 function googleanalytics() {
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
+  if (typeof window !== "undefined") {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-G9VC5S5W87");
   }
-  gtag("js", new Date());
-  gtag("config", "G-G9VC5S5W87");
 }
 
 export default googleanalytics;
