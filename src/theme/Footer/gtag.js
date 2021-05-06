b57ec0334fd8d531233990cca3d6a96420a3216f
Adam <i@adamurban.de>
2021-05-06 07:52:56 +0200

    chore(analytics): add osano and google analytics

---
+++ src/theme/Footer/gtag.js
@@ -0,0 +1,10 @@
+function googleanalytics() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", "G-G9VC5S5W87");
+}
+
+export default googleanalytics;
