e61cb544c4f1346e0afa2a22632973d07be46872
Philipp Ossler <philipp.ossler@gmail.com>
2021-07-07 11:30:39 +0200

    chore(config): enable contextual-search
    * contextual-search is useful for versioned docs to avoid multiple result froms different versions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -137,6 +137,7 @@ module.exports = {
     algolia: {
       apiKey: "f1b2a46296ae374b7f5a5c627341c354",
       indexName: "camunda",
+      contextualSearch: true, // useful for versioned docs (https://docusaurus.io/docs/search#contextual-search)
       searchParameters: {}, // Optional (if provided by Algolia)
     },
     // Disabling Dark Mode
