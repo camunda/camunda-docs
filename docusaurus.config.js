0bf2b7ece9d880baac6f8393f67dad4747a24fa8
Amara <amara.graham@camunda.com>
2022-03-29 16:10:09 -0500

    add C8 banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,13 @@ module.exports = {
   scripts: [
   ],  
   themeConfig: {
+    announcementBar:{
+      id: 'camunda8',
+      content: '<b>📣 Camunda Cloud is now Camunda Platform 8!</b> Learn more about how to navigate the docs <a target="_blank" rel="noopener noreferrer" href="https://docs.camunda.com/meta">here</a>.',
+      backgroundColor: '#14D890',
+      textColor: '#000',
+      isCloseable: false,
+    },
     prism: {
       additionalLanguages: ['java'],
     },
