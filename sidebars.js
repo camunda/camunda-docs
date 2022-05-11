78ba2e45cbb66894810bc781ee56ce76c5eaa7c1
Christina Ausley <christina.ausley@camunda.com>
2022-05-11 16:16:35 -0400

    prettier implementation

--- sidebars.js
+++ sidebars.js
@@ -598,7 +598,7 @@ module.exports = {
       ],
     },
     {
-      "Zeebe Gateway": ["self-managed/zeebe-deployment/index"]
+      "Zeebe Gateway": ["self-managed/zeebe-deployment/index"],
     },
     {
       Troubleshooting: ["self-managed/troubleshooting/log-levels"],
