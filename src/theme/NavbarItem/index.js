5afc36340464d65c9bde6873640c2c3359bdbece
Steven Hicks <steven.hicks@camunda.com>
2022-05-12 15:43:20 -0500

    show correct version dropdown depending on your location in the docs

--- src/theme/NavbarItem/index.js
+++ src/theme/NavbarItem/index.js
@@ -1,53 +1,29 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import React from "react";
-import DefaultNavbarItem from "@theme/NavbarItem/DefaultNavbarItem";
-import DropdownNavbarItem from "@theme/NavbarItem/DropdownNavbarItem";
-import LocaleDropdownNavbarItem from "@theme/NavbarItem/LocaleDropdownNavbarItem";
-import SearchNavbarItem from "@theme/NavbarItem/SearchNavbarItem";
-const NavbarItemComponents = {
-  default: () => DefaultNavbarItem,
-  localeDropdown: () => LocaleDropdownNavbarItem,
-  search: () => SearchNavbarItem,
-  dropdown: () => DropdownNavbarItem,
-  // Need to lazy load these items as we don't know for sure the docs plugin is loaded
-  // See https://github.com/facebook/docusaurus/issues/3360
+import OriginalNavbarItem from "@theme-original/NavbarItem";
 
-  /* eslint-disable @typescript-eslint/no-var-requires, global-require */
-  docsVersion: () => require("@theme/NavbarItem/DocsVersionNavbarItem").default,
-  docsVersionDropdown: () =>
-    require("@theme/NavbarItem/DocsVersionDropdownNavbarItem").default,
-  doc: () => require("@theme/NavbarItem/DocNavbarItem").default,
-  docSidebar: () => require("@theme/NavbarItem/DocSidebarNavbarItem").default,
-  /* eslint-enable @typescript-eslint/no-var-requires, global-require */
-};
+import { useLocation } from "@docusaurus/router";
 
-const getNavbarItemComponent = (type) => {
-  const navbarItemComponentFn = NavbarItemComponents[type];
+export default function NavbarItem(props) {
+  const { type } = props;
+  const { pathname } = useLocation();
 
-  if (!navbarItemComponentFn) {
-    throw new Error(`No NavbarItem component found for type "${type}".`);
+  if (type === "docsVersionDropdown") {
+    let pluginIdForDocsDropdown;
+
+    if (/^\/community/.test(pathname)) {
+      pluginIdForDocsDropdown = "community";
+    }
+
+    return (
+      <>
+        <OriginalNavbarItem {...props} docsPluginId={pluginIdForDocsDropdown} />
+      </>
+    );
   }
 
-  return navbarItemComponentFn();
-};
-
-function getComponentType(type, isDropdown) {
-  // Backward compatibility: navbar item with no type set
-  // but containing dropdown items should use the type "dropdown"
-  if (!type || type === "default") {
-    return isDropdown ? "dropdown" : "default";
-  }
-
-  return type;
-}
-
-export default function NavbarItem({ type, ...props }) {
-  const componentType = getComponentType(type, props.items !== undefined);
-  const NavbarItemComponent = getNavbarItemComponent(componentType);
-  return <NavbarItemComponent {...props} />;
+  return (
+    <>
+      <OriginalNavbarItem {...props} />
+    </>
+  );
 }
