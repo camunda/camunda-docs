e343d482f87da784d494b0f628aa891a861ff851
Beriwan Tepe <beriwan.tepe@camunda.com>
2022-06-10 17:41:54 +0200

    chore(identity): add user guide for assigning permissions to applications and introduce concept

--- sidebars.js
+++ sidebars.js
@@ -582,6 +582,7 @@ module.exports = {
             "self-managed/identity/user-guide/adding-an-application",
             "self-managed/identity/user-guide/adding-an-api",
             "self-managed/identity/user-guide/adding-a-permission",
+            "self-managed/identity/user-guide/assigning-a-permission-to-an-application",
             "self-managed/identity/user-guide/adding-a-role",
             "self-managed/identity/user-guide/assigning-a-permission-to-a-role",
             "self-managed/identity/user-guide/assigning-a-role-to-a-user",
