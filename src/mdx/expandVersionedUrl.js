430bb88a5f7d7fe7d659e9edbea5db5ec2d2561d
Steven Hicks <steven.hicks@camunda.com>
2022-08-17 11:25:14 -0500

    feat: support auto-expanding of $ token in markdown links

--- src/mdx/expandVersionedUrl.js
+++ src/mdx/expandVersionedUrl.js
@@ -5,12 +5,24 @@ const docs = [
   { match: "/optimize_versioned_docs/version-3.7.0/", expandTo: "/docs/1.3" },
 ];
 
+const optimize = [
+  { match: "/docs/", expandTo: "/optimize/next" },
+  { match: "/versioned_docs/version-8.0/", expandTo: "/optimize" },
+  { match: "/versioned_docs/version-1.3/", expandTo: "/optimize/3.7.0" },
+];
+
 function expandVersionedUrl(targetUrl, sourcePath) {
   let expandedUrl = targetUrl;
 
   docs.forEach((rule) => {
     if (sourcePath.includes(rule.match)) {
       expandedUrl = targetUrl.replace("$docs$", rule.expandTo);
+    }
+  });
+
+  optimize.forEach((rule) => {
+    if (sourcePath.includes(rule.match)) {
+      expandedUrl = targetUrl.replace("$optimize$", rule.expandTo);
     }
   });
 
--- src/mdx/expandVersionedUrl.spec.js
+++ src/mdx/expandVersionedUrl.spec.js
@@ -40,9 +40,23 @@ describe("expandVersionedUrl", () => {
   });
 
   describe("when source is from main docs", () => {
-    // it("expands $optimize$ to /optimize", () => {
-    //   const url = "$optimize$/some/thing";
-    //   expect(expandVersionedUrl(url)).toEqual("/optimize/some/thing");
-    // });
+    const targetUrl = "$optimize$/some/thing";
+
+    it.each([
+      [
+        "/Users/monkeypants/camunda-platform-docs/docs/what-is-optimize.md",
+        "/optimize/next/some/thing",
+      ],
+      [
+        "/Users/monkeypants/camunda-platform-docs/versioned_docs/version-8.0/what-is-optimize.md",
+        "/optimize/some/thing",
+      ],
+      [
+        "/Users/monkeypants/camunda-platform-docs/versioned_docs/version-1.3/what-is-optimize.md",
+        "/optimize/3.7.0/some/thing",
+      ],
+    ])("when in %s it expands to %s", (sourcePath, expandedUrl) => {
+      expect(expandVersionedUrl(targetUrl, sourcePath)).toEqual(expandedUrl);
+    });
   });
 });
