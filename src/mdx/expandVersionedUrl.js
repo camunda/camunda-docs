526aaa2103b787ef315acc42577a9d48e00ce244
Steven Hicks <steven.hicks@camunda.com>
2022-08-23 11:28:48 -0500

    wip: map optimize/ to docs/next and docs/next to optimize, because there are no optimize versions yet

--- src/mdx/expandVersionedUrl.js
+++ src/mdx/expandVersionedUrl.js
@@ -2,8 +2,8 @@ const tokens = [
   {
     token: "$docs$",
     rules: [
-      // these mappings are currently wrong because there are no optimize versions yet...but eventually, this is what we'll want.
       { match: "/optimize/", expandTo: "/docs/next" },
+      // these other mappings are currently wrong because there are no optimize versions yet...but eventually, this is what we'll want.
       { match: "/optimize_versioned_docs/version-3.8.0/", expandTo: "/docs" },
       {
         match: "/optimize_versioned_docs/version-3.7.0/",
@@ -14,10 +14,11 @@ const tokens = [
   {
     token: "$optimize$",
     rules: [
-      // these mappings are currently wrong because there are no optimize versions yet...but eventually, this is what we'll want.
-      { match: "/docs/", expandTo: "/optimize/next" },
-      { match: "/versioned_docs/version-8.0/", expandTo: "/optimize" },
-      { match: "/versioned_docs/version-1.3/", expandTo: "/optimize/3.7.0" },
+      { match: "/docs/", expandTo: "/optimize" },
+      // TODO: introduce these mappings, as versions are added for optimize.
+      // { match: "/docs/", expandTo: "/optimize/next" },
+      // { match: "/versioned_docs/version-8.0/", expandTo: "/optimize" },
+      // { match: "/versioned_docs/version-1.3/", expandTo: "/optimize/3.7.0" },
     ],
   },
 ];
