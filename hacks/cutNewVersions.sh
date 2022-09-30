081672017f324141b72046dd5ddcc37daf873215
Steven Hicks <steven.hicks@camunda.com>
2022-09-30 17:02:28 -0500

    feat: progress indication in versioning script

--- hacks/cutNewVersions.sh
+++ hacks/cutNewVersions.sh
@@ -14,17 +14,17 @@ echo $NEW_OPTIMIZE_VERSION
 echo $PREVIOUS_OPTIMIZE_VERSION
 
 
-echo "1/ Creating version $NEW_DOCS_VERSION of main docs..."
+echo "1/4 Creating version $NEW_DOCS_VERSION of main docs..."
 npm run docusaurus docs:version $NEW_DOCS_VERSION
 
-echo "2/ Creating version $NEW_OPTIMIZE_VERSION of Optimize docs..."
+echo "2/4 Creating version $NEW_OPTIMIZE_VERSION of Optimize docs..."
 npm run docusaurus docs:version:optimize $NEW_OPTIMIZE_VERSION
 
-echo "3/ Correcting sidebars files of new versions..."
+echo "3/4 Correcting sidebars files of new versions..."
 sed -i '' 's/\"href\": \"\/optimize\/next\//\"href\": \"\/optimize\//g' versioned_sidebars/version-$NEW_DOCS_VERSION-sidebars.json
 sed -i '' 's/\"href\": \"\/docs\/next\//\"href\": \"\/docs\//g' optimize_versioned_sidebars/version-$NEW_OPTIMIZE_VERSION-sidebars.json
 
-echo "4/ Correcting sidebars files of previous versions..."
+echo "4/4 Correcting sidebars files of previous versions..."
 sed -i '' "s/\"href\": \"\/optimize\//\"href\": \"\/optimize\/${PREVIOUS_OPTIMIZE_VERSION}\//g" versioned_sidebars/version-$PREVIOUS_DOCS_VERSION-sidebars.json
 sed -i '' "s/\"href\": \"\/docs\//\"href\": \"\/docs\/${PREVIOUS_DOCS_VERSION}\//g" optimize_versioned_sidebars/version-$PREVIOUS_OPTIMIZE_VERSION-sidebars.json
 
