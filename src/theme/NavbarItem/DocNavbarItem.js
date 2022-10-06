1a75877dc99a80a9a8220cffca88961a71a08111
Steven Hicks <steven.hicks@camunda.com>
2022-10-06 11:35:25 -0500

    refactor: reorder for readability

--- src/theme/NavbarItem/DocNavbarItem.js
+++ src/theme/NavbarItem/DocNavbarItem.js
@@ -9,38 +9,44 @@ import {
 } from "@docusaurus/theme-common/internal";
 import DefaultNavbarItem from "@theme/NavbarItem/DefaultNavbarItem";
 
-// Note that the original method is memoized, and we will likely want to do the same.
-//   (I can see logs from this function a dozen times per page load, due to it running for every nav link.)
-// Based on https://github.com/facebook/docusaurus/blob/abe545052693bfab98b347c14f3e00709bb2ac06/packages/docusaurus-theme-common/src/utils/docsUtils.tsx#L203-L216
-function useDocsVersionCandidatesAlt(docsPluginId, optimizeActiveDocContext) {
-  // 1. Let docusaurus determine the version candidates -- it orders them based on what it thinks the user wants.
-  //   Note that it's non-deterministic -- because it's based on what you've browsed recently.
-  const versions = useDocsVersionCandidates(docsPluginId);
+// TODO: get this from the original instead of duplicating here
+const versionMappings = [
+  {
+    docsVersion: "8.0",
+    optimizeVersion: "3.8.0",
+  },
+  { docsVersion: "1.3", optimizeVersion: "3.7.0" },
+];
 
-  // 2. But we also know which optimize versions align with which docs versions,
-  //   so let's find the docs version that aligns with this current optimize doc...
-  const activeOptimizeVersion = optimizeActiveDocContext.activeVersion.name;
-  const activeVersionMapping = versionMappings.find(
-    (x) => x.optimizeVersion === activeOptimizeVersion
-  );
-  // "current" is what docusaurus calls the "next" version. Naming, amiright?
-  const activeDocsVersion = activeVersionMapping?.docsVersion || "current";
+// Swizzled from https://github.com/facebook/docusaurus/blob/abe545052693bfab98b347c14f3e00709bb2ac06/packages/docusaurus-theme-classic/src/theme/NavbarItem/DocNavbarItem.tsx
+export default function DocNavbarItem({
+  docId,
+  label: staticLabel,
+  docsPluginId,
+  ...props
+}) {
+  const { activeDoc } = useActiveDocContext(docsPluginId);
 
-  // 3. Docusaurus might not have included the mapped version in its candidates!
-  const index = versions.findIndex((x) => x.name === activeDocsVersion);
-  console.log("versions", versions, index);
-  if (index !== -1) {
-    // if it _did_ include it, we can bump the mapped version to the top of the candidates:
-    // thanks, stackoverflow! https://stackoverflow.com/a/48456512
-    versions.unshift(versions.splice(index, 1)[0]);
-  } else {
-    // if it didn't include it, we can load the version ourselves, and prepend it to the top of the candidates:
-    const allVersions = useVersions(docsPluginId);
-    const nextVersion = allVersions.find((x) => x.name === "current");
-    versions.unshift(nextVersion);
+  // 👋 this call is the only thing that changes from the original DocNavbarItem
+  // const doc = useLayoutDoc(docId, docsPluginId);
+  const doc = useLayoutDocAlt(docId, docsPluginId);
+
+  // Draft items are not displayed in the navbar.
+  if (doc === null) {
+    return null;
   }
-
-  return versions;
+  return (
+    <DefaultNavbarItem
+      exact
+      {...props}
+      isActive={() =>
+        activeDoc?.path === doc.path ||
+        (!!activeDoc?.sidebar && activeDoc.sidebar === doc.sidebar)
+      }
+      label={staticLabel ?? doc.id}
+      to={doc.path}
+    />
+  );
 }
 
 // Based on https://github.com/facebook/docusaurus/blob/c811d6249e0afc23a41b7e54f88f8ce43a3ec358/packages/docusaurus-theme-common/src/utils/docsUtils.tsx#L290-L316
@@ -86,42 +92,36 @@ Available doc ids are:
   }, [docId, versions]);
 }
 
-// TODO: get this from the original instead of duplicating here
-const versionMappings = [
-  {
-    docsVersion: "8.0",
-    optimizeVersion: "3.8.0",
-  },
-  { docsVersion: "1.3", optimizeVersion: "3.7.0" },
-];
+// Note that the original method is memoized, and we will likely want to do the same.
+//   (I can see logs from this function a dozen times per page load, due to it running for every nav link.)
+// Based on https://github.com/facebook/docusaurus/blob/abe545052693bfab98b347c14f3e00709bb2ac06/packages/docusaurus-theme-common/src/utils/docsUtils.tsx#L203-L216
+function useDocsVersionCandidatesAlt(docsPluginId, optimizeActiveDocContext) {
+  // 1. Let docusaurus determine the version candidates -- it orders them based on what it thinks the user wants.
+  //   Note that it's non-deterministic -- because it's based on what you've browsed recently.
+  const versions = useDocsVersionCandidates(docsPluginId);
 
-// Swizzled from https://github.com/facebook/docusaurus/blob/abe545052693bfab98b347c14f3e00709bb2ac06/packages/docusaurus-theme-classic/src/theme/NavbarItem/DocNavbarItem.tsx
-export default function DocNavbarItem({
-  docId,
-  label: staticLabel,
-  docsPluginId,
-  ...props
-}) {
-  const { activeDoc } = useActiveDocContext(docsPluginId);
-
-  // 👋 this call is the only thing that changes from the original DocNavbarItem
-  // const doc = useLayoutDoc(docId, docsPluginId);
-  const doc = useLayoutDocAlt(docId, docsPluginId);
-
-  // Draft items are not displayed in the navbar.
-  if (doc === null) {
-    return null;
-  }
-  return (
-    <DefaultNavbarItem
-      exact
-      {...props}
-      isActive={() =>
-        activeDoc?.path === doc.path ||
-        (!!activeDoc?.sidebar && activeDoc.sidebar === doc.sidebar)
-      }
-      label={staticLabel ?? doc.id}
-      to={doc.path}
-    />
+  // 2. But we also know which optimize versions align with which docs versions,
+  //   so let's find the docs version that aligns with this current optimize doc...
+  const activeOptimizeVersion = optimizeActiveDocContext.activeVersion.name;
+  const activeVersionMapping = versionMappings.find(
+    (x) => x.optimizeVersion === activeOptimizeVersion
   );
+  // "current" is what docusaurus calls the "next" version. Naming, amiright?
+  const activeDocsVersion = activeVersionMapping?.docsVersion || "current";
+
+  // 3. Docusaurus might not have included the mapped version in its candidates!
+  const index = versions.findIndex((x) => x.name === activeDocsVersion);
+  console.log("versions", versions, index);
+  if (index !== -1) {
+    // if it _did_ include it, we can bump the mapped version to the top of the candidates:
+    // thanks, stackoverflow! https://stackoverflow.com/a/48456512
+    versions.unshift(versions.splice(index, 1)[0]);
+  } else {
+    // if it didn't include it, we can load the version ourselves, and prepend it to the top of the candidates:
+    const allVersions = useVersions(docsPluginId);
+    const nextVersion = allVersions.find((x) => x.name === "current");
+    versions.unshift(nextVersion);
+  }
+
+  return versions;
 }
