0d29d63b6a89bd7489bdfe00a41a38c92a16c067
Andromachi Rozaki <andromachi.rozaki@camunda.com>
2022-11-29 13:56:48 +0100

    docs(ml-dataset): Update the user guide so that the json export and the new columns on the raw data report are being documented
    relates to: OPT-6424

--- sidebars.js
+++ sidebars.js
@@ -437,6 +437,10 @@ module.exports = {
                   "Process variants comparison",
                   "components/userguide/additional-features/process-variants-comparison/"
                 ),
+                optimizeLink(
+                  "Machine Learning Ready Dataset",
+                  "components/userguide/additional-features/ml-dataset/"
+                ),
               ],
             },
           ],
