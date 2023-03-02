326102e9616d4d0b6b969142dd06e6f8ceb527c8
Remco <remco@westerhoud.nl>
2023-03-02 09:58:10 +0100

    Add Escalation Events to docs (#1750)
    * fix: typo in error event description
    
    * docs(bpmn): add BPMN escalation events (#1531)
    
    * style(formatting): technical review
    
    ---------
    
    Co-authored-by: Christina Ausley <christina.ausley@camunda.com>
    Co-authored-by: christinaausley <84338309+christinaausley@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -194,6 +194,7 @@ module.exports = {
                 "components/modeler/bpmn/message-events/message-events",
                 "components/modeler/bpmn/timer-events/timer-events",
                 "components/modeler/bpmn/error-events/error-events",
+                "components/modeler/bpmn/escalation-events/escalation-events",
                 "components/modeler/bpmn/terminate-events/terminate-events",
               ],
             },
