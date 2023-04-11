7464b7cdf3ca5ca0477e8d89204e719575ce72d8
Steven Hicks <steven.hicks@camunda.com>
2023-04-10 19:02:36 -0500

    temp: disable old versions to speed up build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -222,11 +222,11 @@ module.exports = {
             "https://github.com/camunda/camunda-platform-docs/edit/main/",
           beforeDefaultRemarkPlugins: [versionedLinks],
           // 👋 When cutting a new version, remove the banner for maintained versions by adding an entry. Remove the entry to versions >18 months old.
-          versions: {
-            "8.0": {
-              banner: "none",
-            },
-          },
+          // versions: {
+          //   "8.0": {
+          //     banner: "none",
+          //   },
+          // },
         },
         blog: false,
         theme: {
