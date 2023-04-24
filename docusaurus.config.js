dd2e1315cd924577cb44f6fd76ec11470bc99d17
Steven Hicks <steven.hicks@camunda.com>
2023-04-24 14:21:58 -0500

    hack: temporarily remove old versions so builds go faster during experiment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,15 @@ module.exports = {
         beforeDefaultRemarkPlugins: [versionedLinks],
         sidebarPath: require.resolve("./optimize_sidebars.js"),
         editUrl: "https://github.com/camunda/camunda-platform-docs/edit/main/",
-        versions: {
-          "3.9.0": {
-            banner: "none",
-          },
-          "3.8.0": {
-            banner: "none",
-          },
-        },
+        // temporarily removed
+        // versions: {
+        //   "3.9.0": {
+        //     banner: "none",
+        //   },
+        //   "3.8.0": {
+        //     banner: "none",
+        //   },
+        // },
       },
     ],
   ],
@@ -230,14 +231,15 @@ module.exports = {
             "https://github.com/camunda/camunda-platform-docs/edit/main/",
           beforeDefaultRemarkPlugins: [versionedLinks],
           // 👋 When cutting a new version, remove the banner for maintained versions by adding an entry. Remove the entry to versions >18 months old.
-          versions: {
-            8.1: {
-              banner: "none",
-            },
-            "8.0": {
-              banner: "none",
-            },
-          },
+          // temporarily removed
+          // versions: {
+          //   8.1: {
+          //     banner: "none",
+          //   },
+          //   "8.0": {
+          //     banner: "none",
+          //   },
+          // },
         },
         blog: false,
         theme: {
