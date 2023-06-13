f6803dba2ee807f24a280dd3de99a4ef46a5aa1b
Steven Hicks <steven.hicks@camunda.com>
2023-06-13 16:09:17 -0500

    archiving(exclude): move 0.26 to unsupported versions in dropdown

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ module.exports = {
               className: "dropdown-unsupported-versions",
               value: "<b>Unsupported versions</b>",
             },
-            ...["0.25"].map((version) => ({
+            ...["0.25", "0.26"].map((version) => ({
               label: version,
               href: `https://unsupported.docs.camunda.io/${version}/`,
             })),
@@ -264,8 +264,6 @@ module.exports = {
           ignorePatterns: [
             "/docs/**/tags/**",
             "/docs/next/**",
-            "/docs/0.25/**",
-            "/docs/0.26/**",
             "/docs/1.0/**",
             "/docs/1.1/**",
             "/docs/1.2/**",
