078d4697c24ec3807cd6d3b5a941a3b479979684
Juan Pablo <jpguisasola@gmail.com>
2023-06-30 13:04:16 -0300

    add link icon and styles

--- src/theme/Navbar/Search/index.js
+++ src/theme/Navbar/Search/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Search from "@theme-original/Navbar/Search";
+import IconExternalLink from "@theme-original/Icon/ExternalLink";
 
 export default function SearchWrapper(props) {
   return (
     <>
       <a
+        className="create-account-btn"
         target="_blank"
         href="https://signup.camunda.com/accounts?_ga=2.124018762.1966193156.1686668003-663653735.1686668003"
       >
-        Create account
+        Create account <IconExternalLink></IconExternalLink>
       </a>
       <Search {...props} />
     </>
