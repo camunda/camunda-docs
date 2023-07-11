55d7c5f3f738a02338de1b80ae04b35b0d335b86
Amara Graham <akeller@users.noreply.github.com>
2023-07-11 16:58:51 -0500

    Add social logos to footer (#2315)
    * add social logos to footer
    
    * Add alt tags
    
    Co-authored-by: Steven Hicks <steven.j.hicks@gmail.com>
    
    * Fix src links
    
    Co-authored-by: Steven Hicks <steven.j.hicks@gmail.com>
    
    ---------
    
    Co-authored-by: Steven Hicks <steven.j.hicks@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -146,16 +146,7 @@ module.exports = {
           title: "Community",
           items: [
             {
-              label: "Slack",
-              href: "https://camunda.com/slack",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/camunda",
-            },
-            {
-              label: "GitHub",
-              href: "https://github.com/camunda/camunda-platform-docs",
+              html: `<a href="https://camunda.com/slack" target="_blank" rel="noreferrer noopener"><img src= "/img/Slack-mark-white-RGB.png" alt="Camunda on Slack" class="footer-logos" /></a> <a href="https://twitter.com/camunda" target="_blank" rel="noreferrer noopener"><img src= "/img/twitter.svg" alt="Camunda on Twitter" class="footer-logos" /></a> <a href="https://github.com/camunda" target="_blank" rel="noreferrer noopener"><img src= "/img/github-mark-white.svg" alt="Camunda on GitHub" class="footer-logos" /></a>`,
             },
             {
               label: "Forum",
