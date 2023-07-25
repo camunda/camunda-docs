5ee6931bb861c807b050bcb49a403b617135855a
Amara <amara.graham@camunda.com>
2023-07-25 15:49:24 -0500

    fix asset ignorePatterns

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -245,7 +245,7 @@ module.exports = {
           changefreq: "weekly",
           priority: 0.5,
           ignorePatterns: [
-            "/assets/**",
+            "/docs/**/assets/**",
             "/docs/**/tags/**",
             "/docs/next/**",
             "/docs/1.1/**",
