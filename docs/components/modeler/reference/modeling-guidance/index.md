7db2f815ebe0c8a49e8974d2b85901099f325552
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-08-08 17:14:03 +0200

    WIP: move `rules` under `modeling guidance`

---
+++ docs/components/modeler/reference/modeling-guidance/index.md
@@ -0,0 +1,17 @@
+---
+id: index
+title: Modeling guidance
+sidebar_label: About
+description: Learn more about called elements, element types, error and escalation reference, and more.
+---
+
+Modeler checks your BPMN diagram for problems affecting deployment and execution, and provides guidance on how to fix them. This page lists all the rules used to check your diagram and explains how to fix the problems.
+
+## Rules
+
+- [Called element](/docs/components/modeler/reference/modeling-guidance/rules/called-element.md)
+- [Element type](/docs/components/modeler/reference/modeling-guidance/rules/element-type.md)
+- [Error reference](/docs/components/modeler/reference/modeling-guidance/rules/error-reference.md)
+- [Escalation reference](/docs/components/modeler/reference/modeling-guidance/rules/escalation-reference.md)
+- [FEEL](/docs/components/modeler/reference/modeling-guidance/rules/feel.md)
+- [Message reference](/docs/components/modeler/reference/modeling-guidance/rules/message-reference.md)
--- docs/components/modeler/reference/sidebar-schema.js
+++ docs/components/modeler/reference/sidebar-schema.js
@@ -1,19 +1,17 @@
 module.exports = {
-  Reference: [
-    {
-      "Modeling guidance": [
-        "components/modeler/reference/about-modeling-guidance",
-        {
-          Rules: [
-            "components/modeler/reference/rules/called-element",
-            "components/modeler/reference/rules/element-type",
-            "components/modeler/reference/rules/error-reference",
-            "components/modeler/reference/rules/escalation-reference",
-            "components/modeler/reference/rules/feel",
-            "components/modeler/reference/rules/message-reference",
-          ],
-        },
-      ],
-    },
-  ],
+  Reference: {
+    "Modeling Guidance": [
+      "components/modeler/reference/modeling-guidance/index",
+      {
+        Rules: [
+          "components/modeler/reference/modeling-guidance/rules/called-element",
+          "components/modeler/reference/modeling-guidance/rules/element-type",
+          "components/modeler/reference/modeling-guidance/rules/error-reference",
+          "components/modeler/reference/modeling-guidance/rules/escalation-reference",
+          "components/modeler/reference/modeling-guidance/rules/feel",
+          "components/modeler/reference/modeling-guidance/rules/message-reference",
+        ],
+      },
+    ],
+  },
 };
