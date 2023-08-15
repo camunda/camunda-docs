b9874dc43256c21aec74b0ca8dc6330bde889fdb
Philipp Fromme <philippfromme@outlook.com>
2023-08-15 17:55:59 +0200

    feat(modeler): add inital modeling guidance documentation (#2359)
    Related to https://github.com/camunda/product-hub/issues/1446
    
    Co-authored-by: christinaausley <84338309+christinaausley@users.noreply.github.com>
    Co-authored-by: Nico Rehwaldt <nico.rehwaldt@camunda.com>

---
+++ docs/components/modeler/reference/sidebar-schema.js
@@ -0,0 +1,17 @@
+module.exports = {
+  Reference: {
+    "Modeling guidance": [
+      "components/modeler/reference/modeling-guidance/index",
+      {
+        Rules: [
+          "components/modeler/reference/modeling-guidance/rules/called-element",
+          "components/modeler/reference/modeling-guidance/rules/element-type",
+          "components/modeler/reference/modeling-guidance/rules/error-reference",
+          "components/modeler/reference/modeling-guidance/rules/escalation-reference",
+          "components/modeler/reference/modeling-guidance/rules/feel",
+          "components/modeler/reference/modeling-guidance/rules/message-reference",
+        ],
+      },
+    ],
+  },
+};
