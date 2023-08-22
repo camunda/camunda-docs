fc767cc09ad93ca04d99550e6981d16d73552fa7
Christina Ausley <christina.ausley@camunda.com>
2023-08-22 15:42:51 -0400

    adjust desktop modeler link

--- sidebars.js
+++ sidebars.js
@@ -184,7 +184,7 @@ module.exports = {
           items: [
             "components/modeler/desktop-modeler/install-the-modeler",
             "components/modeler/desktop-modeler/model-your-first-diagram",
-            "components/modeler/desktop-modeler/connect-to-camunda-cloud",
+            "components/modeler/desktop-modeler/connect-to-camunda-platform-8",
             "components/modeler/desktop-modeler/start-instance",
             {
               type: "category",
