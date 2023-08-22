42d5aa63c188bac7ab0ce172cb8af7c89b5c32a7
Steven Hicks <steven.hicks@camunda.com>
2023-08-22 15:30:20 -0500

    feat: take optimize into account when deleting other versions

--- hacks/isolateVersion/1-deleteOtherVersions.sh
+++ hacks/isolateVersion/1-deleteOtherVersions.sh
@@ -1,30 +1,13 @@
 function delete_version() {
-  notify "Deleting version $1..."
-  folder=versioned_docs/$VERSION
-  sidebars=versioned_sidebars/$VERSION-sidebars.json
+  local version_name=${1}
+  local folder=${2}
+  local sidebars=${3}
+
   rm -rf $folder
   rm $sidebars
-  git add versioned_docs versioned_sidebars
-  git commit -m "archiving: delete version $VERSION"
-}
-
-function delete_next() {
-  notify "Deleting next version..."
-  folder=docs
-  sidebars=sidebars.js
-  rm -rf $folder
-  rm $sidebars
-  git add docs sidebars.js
-  git commit -m "archiving: delete version 'next'"  
-}
-
-function delete_optimize() {
-  notify "Deleting Optimize docs..."
-  rm -rf ./optimize
-  rm -rf ./optimize_versioned_docs
-  rm -rf ./optimize_versioned_sidebars
-  git add optimize optimize_versioned_docs optimize_versioned_sidebars
-  git commit -m "archiving: delete optimize docs"  
+  # This is all of the possible things that could have been deleted.
+  git add $folder $sidebars
+  git commit -m "archiving($ARCHIVED_VERSION): delete version $version_name"  
 }
 
 # list all versions; search for all that don't match the current version; continue even if there are no matches.
@@ -32,9 +15,20 @@ OTHER_VERSIONS=$(ls versioned_docs | grep -xv "version-$ARCHIVED_VERSION" || tru
 
 for VERSION in ${OTHER_VERSIONS[*]}
   do
-    delete_version $VERSION
+    delete_version "docs/$VERSION" "versioned_docs/$VERSION" "versioned_sidebars/$VERSION-sidebars.json"
   done
 
-delete_next
+# delete docs/next version
+delete_version "docs/next" "docs" "sidebars.js"
 
-delete_optimize
+# list all *optimize* versions; search for all that don't match the current version; continue even if there are no matches.
+OTHER_OPTIMIZE_VERSIONS=$(ls optimize_versioned_docs | grep -xv "version-$ARCHIVED_OPTIMIZE_VERSION" || true) 
+
+for VERSION in ${OTHER_OPTIMIZE_VERSIONS[*]}
+  do
+    delete_version "optimize/$VERSION" "optimize_versioned_docs/$VERSION" "optimize_versioned_sidebars/$VERSION-sidebars.json"
+  done
+
+# delete optimize/next version
+delete_version "optimize/next" "optimize" "optimize_sidebars.js"
+
