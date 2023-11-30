53171c4f078763f3abd5e4f3fbcef9726b812b9a
Lars Lange <9141483+Langleu@users.noreply.github.com>
2023-11-30 10:53:17 +0100

    chore(aws): add first iteration on eksctl usage (#2826)
    * chore(aws): add first iteration on eksctl usage
    
    * chore(aws): address first review adjustments for eksctl
    
    * chore(aws): rework eksctl docs by automating a lot of things using filters and queries
    
    * chore(aws): add iam eks requirements to eksctl docs
    
    * chore(aws): add helm iteration to eksctl guide (#2933)
    
    * chore(aws): add IAM setup required for eksctl
    
    * docs(aws): add storage class requirements
    
    * First review (#2957)
    
    * First review
    
    * docs(aws): fix formating issues
    
    * docs(aws): small fixes to the eksctl review
    
    ---------
    
    Co-authored-by: Langleu <lars.lange@camunda.com>
    
    * docs(aws): address review feedback
    
    drop patch versions, add postgres version var, aws org id var, iam setup optional
    
    ---------
    
    Co-authored-by: Andrey Belik <belika@hotmail.com>
    
    ---------
    
    Co-authored-by: Andrey Belik <belika@hotmail.com>

--- optimize_sidebars.js
+++ optimize_sidebars.js
@@ -1741,6 +1741,10 @@ module.exports = {
                 {
                   "Amazon EKS": [
                     docsLink(
+                      "EKS Eksctl Setup",
+                      "self-managed/platform-deployment/helm-kubernetes/platforms/amazon-eks/eks-eksctl"
+                    ),
+                    docsLink(
                       "IAM roles for service accounts",
                       "self-managed/platform-deployment/helm-kubernetes/platforms/amazon-eks/irsa/"
                     ),
