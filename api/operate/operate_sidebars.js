f9b8c405752041cba33cce62e40e421ff692cef3
Steven Hicks <steven.j.hicks@gmail.com>
2023-12-13 15:50:15 -0600

    feat: Operate OpenAPI explorer (part 2) (#2944)
    * prototype: configure a single-version docs instance for the operate OpenAPI explorer
    
    * prototype: add Operate API Explorer to APIs & Clients top-nav item
    
    * prototype: extend NavbarItem to display Operate OpenAPI docs plugin version in navbar
    
    * prototype: configure openapi explorer for operate API and generate some pages
    
    * prototype: add verb badges to openapi explorer
    
    * prototype: scrub the URL from my cluster (I think)
    
    * prototype: add link to operate api explorer from existing operate api docs
    
    * prototype: noindex this site so I can deploy it as a preview
    
    * Remove extra lines
    
    It makes me ridiculously happy to be able to do these prettier updates through the GitHub UI.
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Remove outdated comment
    
    * feedback: revert top nav to remove API explorer direct link
    
    * fix: put child objects on the same line as their summary/details expander
    
    * refactor: extract Authentication to its own doc
    
    * refactor: remove content superseded by api explorer
    
    * refactor: lead with the api explorer
    
    * refactor: remove unused operate api intro page (and change entrypoint into api explorer)
    
    * customize sample languages
    
    * add an 'internal link' indicator in sidebar
    
    * add a backlink to explorer sidebar
    
    * update sidebar links for 8.3 and 8.2
    
    * feat: add borders to (almost all) tab buttons that are missing them
    
    * feat: copy next operate-api structure changes to 8.3
    
    * chore: fix URLs
    
    * feat: add scripts for generating OpenAPI interactive explorers
    
    * docs: on the interactive API explorers
    
    * style(formatting): technical review
    
    * feedback: Title Case for API Explorer
    
    * feedback: nudge on code languages
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Christina Ausley <christina.ausley@camunda.com>

---
+++ api/operate/operate_sidebars.js
@@ -0,0 +1,12 @@
+module.exports = {
+  operate: [
+    {
+      type: "link",
+      href: "/docs/apis-tools/operate-api/overview/",
+      label: "Operate API Overview",
+      className: "internalLink",
+    },
+
+    require("./docs/sidebar.js"),
+  ],
+};
