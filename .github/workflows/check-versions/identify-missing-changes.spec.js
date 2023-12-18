af0a7d5afb5687462a68a5d7722d807fd69f53ac
Steven Hicks <steven.hicks@camunda.com>
2023-12-18 16:47:31 -0600

    tests: identify missing changes

--- .github/workflows/check-versions/identify-missing-changes.spec.js
+++ .github/workflows/check-versions/identify-missing-changes.spec.js
@@ -31,20 +31,45 @@ describe("identifyMissingChanges", () => {
 
         const result = identifyMissingChanges(versionConfig, changedFiles);
 
-        console.log(result);
         expect(result.length).toEqual(1);
         expect(result[0].source).toEqual("a/");
         expect(result[0].suggestion).toEqual("b/");
         expect(result[0].files.length).toEqual(1);
       });
 
-      // it("scrubs the source folder from the suggestion", () => {});
+      it("scrubs the source folder from the suggestion", () => {
+        versionConfig = [
+          { version: "next", source: "a/", suggestions: ["b/"] },
+        ];
+        changedFiles = ["a/file.md"];
+
+        const result = identifyMissingChanges(versionConfig, changedFiles);
+
+        expect(result.length).toEqual(1);
+        expect(result[0].files[0]).toEqual("file.md");
+      });
     });
 
-    // describe("when multiple changes are missing", () => {
-    //   it('recommends changes for all fo the missing versions', ()=> {
+    describe("when multiple changes are missing", () => {
+      it("recommends changes for all for the missing versions", () => {
+        versionConfig = [
+          { version: "next", source: "a/", suggestions: ["b/", "c/"] },
+        ];
+        changedFiles = ["a/file.md"];
 
-    //   })
-    // })
+        const result = identifyMissingChanges(versionConfig, changedFiles);
+
+        expect(result.length).toEqual(2);
+        expect(result[0].source).toEqual("a/");
+        expect(result[0].suggestion).toEqual("b/");
+        expect(result[0].files.length).toEqual(1);
+        expect(result[0].files[0]).toEqual("file.md");
+
+        expect(result[1].source).toEqual("a/");
+        expect(result[1].suggestion).toEqual("c/");
+        expect(result[1].files.length).toEqual(1);
+        expect(result[1].files[0]).toEqual("file.md");
+      });
+    });
   });
 });
