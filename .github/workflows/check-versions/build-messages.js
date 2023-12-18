0a83c75df1e4a844cea6fbc3d1375b6ff8c43a8b
Steven Hicks <steven.hicks@camunda.com>
2023-12-18 16:59:07 -0600

    build messages

--- .github/workflows/check-versions/build-messages.js
+++ .github/workflows/check-versions/build-messages.js
@@ -1,3 +1,20 @@
-const missingChanges = process.env.MISSING_CHANGES;
+// @ts-check
 
-console.log("sjhsjhsjh", missingChanges);
+const missingChangesRaw = process.env.MISSING_CHANGES || "";
+
+/** @type {Array<import("./identify-missing-changes").SuggestedChanges>} */
+const missingChanges = JSON.parse(missingChangesRaw.trim());
+
+missingChanges.forEach((missingChange) => {
+  console.log(`
+These files were changed only in *${
+    missingChange.source
+  }*. You might want to duplicate these changes in *${
+    missingChange.suggestion
+  }*.<br/><ul>
+${missingChange.files
+  .map((file) => `<li>${missingChange.source}${file}</li>`)
+  .join("\n")} 
+</ul>
+`);
+});
