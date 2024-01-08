c97b0f884d2c61f436777c08a69a9db390eeb7bc
Steven Hicks <steven.hicks@camunda.com>
2024-01-08 10:51:37 -0600

    enhancement: improve the sidebar generation script by inferring configuration from the URL instead of making the user define it

--- hacks/generateOptimizeSidebars.js
+++ hacks/generateOptimizeSidebars.js
@@ -1,17 +1,33 @@
-const config = {
-  // Set this value based on whether you want to generate a .json file or .js file.
-  //   .js files are better, because we can reduce the number of places we reference the version's base URL,
-  //   but .js is only supported for the "next" version. Docusaurus does not support .js files
-  //   for older/current version sidebars files, so those must use .json.
-  generateJSONOrJS: "JSON", // or "JS"
+// @ts-check
 
-  // Set this prefix based on which version of docs you're viewing
-  //   when generating the Optimize sidebars.
-  //   It will be removed from all generated URLs, so that the version can be
-  //   prepended in one place instead of every single link.
-  //   NOTE: this only has an effect when generating .js files.
-  docsAndVersionUrlPrefix: "/docs/next/",
+// Note: these type definitions are only checked within an editor.
+/**
+ * @typedef {object} Strategy
+ * @property {"JSON" | "JS"} generateJSONOrJS
+ * @property {string} [jsLinkFunctionName]
+ * @property {string} [docsAndVersionUrlPrefix]
+ */
+
+// The strategy for generating the sidebars can be inferred from your current URL.
+/** @type { Record<string, Strategy> } */
+const strategies = {
+  "/docs/next/": {
+    generateJSONOrJS: "JS",
+    jsLinkFunctionName: "docsLink",
+    docsAndVersionUrlPrefix: "/docs/next/",
+  },
+  "/optimize/next/": {
+    generateJSONOrJS: "JS",
+    jsLinkFunctionName: "optimizeLink",
+    docsAndVersionUrlPrefix: "/optimize/next/",
+  },
+  "/docs/": { generateJSONOrJS: "JSON" },
+  "/optimize/": { generateJSONOrJS: "JSON" },
 };
+
+// Defining this globally, to avoid passing it into basically every function.
+/** @type Strategy */
+let strategy;
 
 // From an anchor in the sidebar, generates a call to a helper function
 //   based on the link text and path. See optimize_sidebars.js for what
@@ -19,10 +35,9 @@ const config = {
 //   This version is only usable for generating the "next" version,
 //   because Docusaurus does not support .js files for older/current version sidebars files.
 function mapToDocsLinkJs(a) {
-  return `docsComponentsLink("${a.innerText}", "${a.pathname.replace(
-    config.docsAndVersionUrlPrefix,
-    ""
-  )}"),`;
+  return `${strategy.jsLinkFunctionName}("${
+    a.innerText
+  }", "${a.pathname.replace(strategy.docsAndVersionUrlPrefix, "")}"),`;
 }
 
 // From an anchor in the sidebar, generates a sidebar item JSON object,
@@ -57,7 +72,7 @@ function buildCategory(li) {
 // From an individual page in the sidebar, generates a sidebar definition.
 function buildLink(li) {
   const mapToDocsLink =
-    config.generateJSONOrJS === "JSON" ? mapToDocsLinkJson : mapToDocsLinkJs;
+    strategy.generateJSONOrJS === "JSON" ? mapToDocsLinkJson : mapToDocsLinkJs;
 
   const anchor = li.querySelector(":scope > .menu__link");
   return mapToDocsLink(anchor);
@@ -74,22 +89,70 @@ function buildLi(li) {
   }
 }
 
-// The root of the sidebar menu.
-const root = document.querySelector(".theme-doc-sidebar-menu");
-
 // Generates the sidebar definitions for the entire menu.
 function buildSidebars() {
+  // The root of the sidebar menu.
+  const root = document.querySelector(".theme-doc-sidebar-menu");
+  if (root === null) {
+    throw new Error("Unable to find sidebar menu root.");
+  }
+
   const items = Array.from(root.querySelectorAll(":scope > li"));
 
   const mappedItems = items.map(buildLi);
 
-  return `{
-    "Components": [
+  return `[
       ${mappedItems.join("\n")}
-    ]
-  }
-  `;
+    ]`;
 }
 
-// Copies the generated sidebar definitions to the clipboard.
-copy(buildSidebars());
+// Identifies configuration strategy based on current URL.
+function identifyStrategyFromPath() {
+  const path = window.location.pathname;
+  const strategyKey = Object.keys(strategies).find((strategyPath) =>
+    path.startsWith(strategyPath)
+  );
+
+  if (strategyKey === undefined) {
+    throw new Error(
+      "I'm not sure how to generate sidebars for this URL. Are you sure you're on the correct page?"
+    );
+  }
+  strategy = strategies[strategyKey];
+}
+
+// Expands all sidebar items (so that we can walk them all in the DOM).
+async function expandAllSidebarItems() {
+  const count = () =>
+    document.querySelectorAll(".menu__link--sublist[aria-expanded=false]")
+      .length;
+  const expand = () =>
+    document
+      .querySelectorAll(".menu__link--sublist[aria-expanded=false]")
+      /** @ts-ignore */
+      .forEach((x) => x.click());
+  async function expandAll() {
+    while (count() > 0) {
+      expand();
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+  }
+  await expandAll();
+}
+
+// Store a reference to the copy function globally, because sometimes it's not available from within a promise.
+/** @ts-ignore */
+const copyToClipboard = copy;
+
+// This is the mainline function for generating the sidebar definitions.
+async function generateSidebarDefinition() {
+  identifyStrategyFromPath();
+
+  await expandAllSidebarItems();
+
+  const sidebarDefinition = buildSidebars();
+
+  copyToClipboard(sidebarDefinition);
+
+  console.log("Copied to clipboard!");
+}
