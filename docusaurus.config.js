5fe9fcbceb8da939ddd81e1a21dc5b488d37f65f
Christina Ausley <christina.ausley@camunda.com>
2024-02-09 11:33:03 -0500

    fix footer link to modeler

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -244,7 +244,7 @@ module.exports = {
           title: "Camunda",
           items: [
             {
-              label: "Console",
+              label: "Web Modeler",
               href: "https://camunda.io",
             },
             {
