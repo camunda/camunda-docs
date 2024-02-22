f423ff61edfc4f9d5792502e332e35b94d765403
Steven Hicks <steven.hicks@camunda.com>
2024-02-22 16:24:28 -0600

    docs: simplify action names

--- api/tasklist/docs/sidebar.js
+++ api/tasklist/docs/sidebar.js
@@ -7,7 +7,7 @@ module.exports = [
       {
         type: "doc",
         id: "docs/get-form",
-        label: "Get the form details by form id and processDefinitionKey.",
+        label: "Get form details",
         className: "api-method get",
       },
     ],
@@ -19,7 +19,7 @@ module.exports = [
       {
         type: "doc",
         id: "docs/get-variable-by-id",
-        label: "Get the variable details by variable id.",
+        label: "Get variable details",
         className: "api-method get",
       },
     ],
@@ -31,45 +31,43 @@ module.exports = [
       {
         type: "doc",
         id: "docs/save-draft-task-variables",
-        label: "Saves draft variables for a task.",
+        label: "Save draft variables",
         className: "api-method post",
       },
       {
         type: "doc",
         id: "docs/search-task-variables",
-        label: "Returns the list of task variables",
+        label: "Search task variables",
         className: "api-method post",
       },
       {
         type: "doc",
         id: "docs/search-tasks",
-        label: "Returns the list of tasks that satisfy search request params",
+        label: "Search tasks",
         className: "api-method post",
       },
       {
         type: "doc",
         id: "docs/unassign-task",
-        label: "Unassign a task with provided id. Returns the task.",
+        label: "Unassign a task",
         className: "api-method patch",
       },
       {
         type: "doc",
         id: "docs/complete-task",
-        label:
-          "Complete a task with taskId and optional variables. Returns the task.",
+        label: "Complete a task",
         className: "api-method patch",
       },
       {
         type: "doc",
         id: "docs/assign-task",
-        label: "Assign a task with id to assignee. Returns the task.",
+        label: "Assign a task",
         className: "api-method patch",
       },
       {
         type: "doc",
         id: "docs/get-task-by-id",
-        label:
-          "Get one task by id. Returns task or error when task does not exist.",
+        label: "Get task details",
         className: "api-method get",
       },
     ],
