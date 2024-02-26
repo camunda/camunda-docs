97688ec67881bb474a1ed7986dc55d7d65cc1461
Steven Hicks <steven.hicks@camunda.com>
2024-02-26 15:40:48 -0600

    refactor: rewrite as a plugin for more control over order of scripts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,6 +33,8 @@ module.exports = {
     //            }
     //          },
     //        ],
+    // This custom Osano plugin must precede the gtm-plugin.
+    "./static/plugins/osano",
     [
       require.resolve("docusaurus-gtm-plugin"),
       {
@@ -106,33 +108,6 @@ module.exports = {
         },
       },
     ],
-  ],
-  headTags: [
-    {
-      tagName: "script",
-      innerHTML: `
-        // Required prior to loading osano.js script.
-        window.dataLayer = window.dataLayer || []
-        function gtag() {
-          dataLayer.push(arguments)
-        }
-        gtag('consent', 'default', {
-          ad_storage: 'denied',
-          analytics_storage: 'denied',
-          ad_user_data: 'denied',
-          ad_personalization: 'denied',
-          wait_for_update: 500,
-        })
-        gtag('set', 'ads_data_redaction', true)
-      `,
-      attributes: {},
-    },
-    {
-      tagName: "script",
-      attributes: {
-        src: "https://cmp.osano.com/16CVvwSNKHi9t1grQ/2ce963c0-31c9-4b54-b052-d66a2a948ccc/osano.js",
-      },
-    },
   ],
   scripts: [
     {
