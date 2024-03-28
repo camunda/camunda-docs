78219d083a8a66db486834958e4ce4f76af21b57
Langleu <lars.lange@camunda.com>
2024-03-28 16:15:11 +0100

    docs(dual-region): apply prettier suggestion

--- docs/self-managed/operational-guides/multi-region/components/stateContainer.jsx
+++ docs/self-managed/operational-guides/multi-region/components/stateContainer.jsx
@@ -2,37 +2,38 @@ import React from "react";
 
 import "./stateContainer.css";
 
-export default function StateContainer({ current, desired, description, code }) {
-return (
-<div class="wrapper">
-  <div class="aside left-1">
-    <div>
-      <h5>Current State</h5>
+export default function StateContainer({
+  current,
+  desired,
+  description,
+  code,
+}) {
+  return (
+    <div class="wrapper">
+      <div class="aside left-1">
+        <div>
+          <h5>Current State</h5>
+        </div>
+        <div>{current}</div>
+      </div>
+      <div class="aside right-2">
+        <div>
+          <h5>Desired State</h5>
+        </div>
+        <div>{desired}</div>
+      </div>
+      <div class="below">
+        <div>
+          <h3>Description / Code</h3>
+        </div>
+        <div>
+          {description}
+          {code}
+        </div>
+      </div>
+      <div class="below">
+        <br></br>
+      </div>
     </div>
-    <div>
-      {current}
-    </div>
-  </div>
-  <div class="aside right-2">
-    <div>
-      <h5>Desired State</h5>
-    </div>
-    <div>
-      {desired}
-    </div>
-  </div>
-  <div class="below">
-    <div>
-      <h3>Description / Code</h3>
-    </div>
-    <div>
-      {description}
-      {code}
-    </div>
-  </div>
-  <div class="below">
-    <br></br>
-  </div>
-</div>
-);
+  );
 }
