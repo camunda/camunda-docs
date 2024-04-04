c099d9f3d88e7b7728795cdd324fbbb53cc8500d
Steven Hicks <steven.hicks@camunda.com>
2024-04-04 10:06:46 -0500

    feedback: move specs above migration in left nav

--- docs/apis-tools/tasklist-api-rest/sidebar-schema.js
+++ docs/apis-tools/tasklist-api-rest/sidebar-schema.js
@@ -4,9 +4,9 @@ module.exports = {
   "Tasklist API (REST)": [
     "apis-tools/tasklist-api-rest/tasklist-api-rest-overview",
     "apis-tools/tasklist-api-rest/tasklist-api-rest-authentication",
-    "apis-tools/tasklist-api-rest/migrate-to-zeebe-user-tasks",
     {
       Specifications: require("./specifications/sidebar.js"),
     },
+    "apis-tools/tasklist-api-rest/migrate-to-zeebe-user-tasks",
   ],
 };
