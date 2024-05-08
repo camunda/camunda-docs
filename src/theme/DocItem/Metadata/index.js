0a3d2a5bb886ba96829d10a4085f00bb91ac94c4
Steven Hicks <steven.j.hicks@gmail.com>
2024-05-08 14:14:16 -0500

    feat: support fully-qualified absolute canonical URLs (#3781)
    * refactor: move trailing-slash handling to determineCanonical, to make room for specifying fully-qualified absolute canonical URLs
    
    * feat: support fully qualified canonical URLs

--- src/theme/DocItem/Metadata/index.js
+++ src/theme/DocItem/Metadata/index.js
@@ -24,10 +24,17 @@ export default function MetadataWrapper(props) {
   // From the context, identify the proper canonical
   const canonicalPath = determineCanonical(currentDoc, currentPlugin);
 
-  // Canonical URLs should always:
-  //   1. be fully qualified (Google's recommendation)
-  //   2. end with a trailing slash (to avoid default-document redirects, e.g. /welcome -> /welcome/)
-  const fullCanonicalUrl = `${customFields.canonicalUrlRoot}${canonicalPath}/`;
+  let fullCanonicalUrl;
+  if (/^https?:\/\/.*/i.test(canonicalPath)) {
+    // The canonicalPath set in frontmatter is absolutely qualified. Trust it and use it as-is.
+    fullCanonicalUrl = canonicalPath;
+  } else {
+    // Canonical URLs should always:
+    //   1. be fully qualified (Google's recommendation)
+    //   2. end with a trailing slash (to avoid default-document redirects, e.g. /welcome -> /welcome/)
+    //      The required trailing slash is handled by `determineCanonical`.
+    fullCanonicalUrl = `${customFields.canonicalUrlRoot}${canonicalPath}`;
+  }
 
   return (
     <>
