df3ddb85190d9981d692c76491be0247fe962f97
Adam <adam.urban@gmail.com>
2024-05-15 13:08:38 +0200

    feat(api): improve console sm api docs

--- docs/apis-tools/console-sm-api/sidebar-schema.js
+++ docs/apis-tools/console-sm-api/sidebar-schema.js
@@ -3,6 +3,7 @@
 module.exports = {
   "Console SM API (REST)": [
     "apis-tools/console-sm-api/console-sm-api-overview",
+    "apis-tools/console-sm-api/console-sm-api-authentication",
     {
       Specifications: require("./specifications/sidebar.js"),
     },
