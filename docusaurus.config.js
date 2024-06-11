64526a68dea1685ab15a308664e2292726de6fb5
Andromachi Rozaki <andromachi.rozaki@camunda.com>
2024-06-11 18:04:43 +0200

    chore(preview-envs): Build workflows to deploy and tear down preview envs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,14 +2,20 @@ const versionedLinks = require("./src/mdx/versionedLinks");
 const { unsupportedVersions } = require("./src/versions");
 
 const latestVersion = require("./src/versions").versionMappings[0].docsVersion;
+// Read url and baseUrl from environment variables
+const siteUrlEnv = process.env.DOCS_SITE_URL || "https://docs.camunda.io";
+const siteUrl = siteUrlEnv.startsWith("https://")
+  ? siteUrlEnv
+  : `https://${siteUrlEnv}`;
+const siteBaseUrl = process.env.DOCS_SITE_BASE_URL || "/";
 
 module.exports = {
   title: "Camunda 8 Docs",
   tagline: "Documentation for all components of Camunda 8",
   // url: "https://camunda-cloud.github.io",
-  url: "https://docs.camunda.io",
+  url: siteUrl,
   // baseUrl: "/camunda-cloud-documentation/",
-  baseUrl: "/",
+  baseUrl: siteBaseUrl,
   customFields: {
     canonicalUrlRoot: "https://docs.camunda.io",
   },
