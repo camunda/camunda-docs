dfc34baff88c0a6525a032cc1ecc91d217fe49ac
Christina Ausley <christina.ausley@camunda.com>
2024-07-26 10:58:23 -0400

    remove tasklist and operate apis from deprecated folder

--- sidebars.js
+++ sidebars.js
@@ -673,6 +673,7 @@ module.exports = {
       APIs: [
         require("./docs/apis-tools/administration-api/sidebar-schema"),
         require("./docs/apis-tools/camunda-api-rest/sidebar-schema"),
+        require("./docs/apis-tools/operate-api/sidebar-schema"),
         {
           "Optimize API (REST)": [
             optimizeLink("Overview", "apis-tools/optimize-api/overview/"),
@@ -758,12 +759,11 @@ module.exports = {
             ),
           ],
         },
+        require("./docs/apis-tools/tasklist-api-rest/sidebar-schema"),
         require("./docs/apis-tools/web-modeler-api/sidebar-schema"),
         require("./docs/apis-tools/zeebe-api/sidebar-schema"),
         {
           Deprecated: [
-            require("./docs/apis-tools/operate-api/sidebar-schema"),
-            require("./docs/apis-tools/tasklist-api-rest/sidebar-schema"),
             require("./docs/apis-tools/tasklist-api/sidebar-schema"),
             require("./docs/apis-tools/zeebe-api-rest/sidebar-schema"),
           ],
