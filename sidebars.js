55a2532f4ed245a62dbe5bb2832394b58b07ff19
christinaausley <84338309+christinaausley@users.noreply.github.com>
2024-08-06 07:32:22 -0600

    remove .md from sidebar file

--- sidebars.js
+++ sidebars.js
@@ -305,7 +305,7 @@ module.exports = {
             "components/connectors/out-of-the-box-connectors/automation-anywhere",
             {
               AWS: [
-                "components/connectors/out-of-the-box-connectors/amazon-bedrock.md",
+                "components/connectors/out-of-the-box-connectors/amazon-bedrock",
                 "components/connectors/out-of-the-box-connectors/amazon-dynamodb",
                 "components/connectors/out-of-the-box-connectors/amazon-eventbridge",
                 "components/connectors/out-of-the-box-connectors/aws-lambda",
