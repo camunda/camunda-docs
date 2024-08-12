9bd8e48140f1c6926ec37a0eaa71e43adf24e91b
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-08-12 18:09:24 +0200

    docs(aws-bedrock): AWS bedrock documentation  (#4032)
    * docs(aws-bedrock): add complete documentation on how to use the new aws bedrock connector
    
    * Update docs/components/connectors/out-of-the-box-connectors/amazon-bedrock.md
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update docs/components/connectors/out-of-the-box-connectors/amazon-bedrock.md
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update docs/components/connectors/out-of-the-box-connectors/amazon-bedrock.md
    
    * Update docs/components/connectors/out-of-the-box-connectors/amazon-bedrock.md
    
    * style(formatting): grammatical adjustments
    
    * docs(aws-bedrock): add the bedrock documentation to the sidebars
    
    * Update sidebars.js to alphabetical
    
    * remove .md from sidebar file
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Simon <sbuettner@users.noreply.github.com>
    Co-authored-by: Christina Ausley <christina.ausley@camunda.com>
    Co-authored-by: christinaausley <84338309+christinaausley@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -306,6 +306,7 @@ module.exports = {
             "components/connectors/out-of-the-box-connectors/automation-anywhere",
             {
               AWS: [
+                "components/connectors/out-of-the-box-connectors/amazon-bedrock",
                 "components/connectors/out-of-the-box-connectors/amazon-dynamodb",
                 "components/connectors/out-of-the-box-connectors/amazon-eventbridge",
                 "components/connectors/out-of-the-box-connectors/aws-lambda",
