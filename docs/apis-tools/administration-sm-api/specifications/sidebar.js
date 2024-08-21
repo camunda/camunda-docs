8ffeb49b9aa5833da8c38fcbe0fe960754a7ad3f
Christina Ausley <christina.ausley@camunda.com>
2024-08-21 14:05:40 -0400

    update sidebar id

--- docs/apis-tools/administration-sm-api/specifications/sidebar.js
+++ docs/apis-tools/administration-sm-api/specifications/sidebar.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     type: "doc",
-    id: "apis-tools/administration-sm-api/specifications/sm-administration-api",
+    id: "apis-tools/administration-sm-api/specifications/sm-admin-api",
   },
   {
     type: "category",
