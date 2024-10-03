c80d1139559c6c8dc7a52fec2a7b93bd2cb47aff
Steven Hicks <steven.j.hicks@gmail.com>
2024-10-03 11:57:09 -0500

    feat: redefine union request types when c8 REST docs are generated (#4363)
    * chore: remove unused hack
    
    * fix: add missing import
    
    * feat: redefine createProcessInstanceRequest to express union type
    
    * chore: regenerate docs
    
    * feat: redefine evaluateDecisionRequest to express union type
    
    * chore: regenerate docs
    
    * feat: add a disclaimer to the top of the c8 spec so that people know it doesn't match the source exactly.

--- api/camunda/generation-strategy.js
+++ api/camunda/generation-strategy.js
@@ -2,13 +2,210 @@ const removeDuplicateVersionBadge = require("../remove-duplicate-version-badge")
 const replace = require("replace-in-file");
 const outputDir = "docs/apis-tools/camunda-api-rest/specifications";
 const specFile = "api/camunda/camunda-openapi.yaml";
+const fs = require("fs");
 
 function preGenerateDocs() {
-  hackChangesetDescription();
+  const originalSpec = fs.readFileSync(specFile, "utf8");
+
+  console.log("adjusting C8 spec file...");
+
+  const specUpdates = [
+    addDisclaimer(),
+    ...redefineCreateProcessInstanceRequest(originalSpec),
+    ...redefineEvaluateDecisionRequest(originalSpec),
+  ];
+
+  replace.sync({
+    files: specFile,
+    from: specUpdates.map((x) => x.from),
+    to: specUpdates.map((x) => x.to),
+  });
 }
 
 function postGenerateDocs() {
   removeDuplicateVersionBadge(`${outputDir}/camunda-8-rest-api.info.mdx`);
+}
+
+function addDisclaimer() {
+  // Adds a disclaimer to the very beginning of the file, so that people know this isn't the true spec.
+  return {
+    from: /^/,
+    to: `# Disclaimer: This is a modified version of the Camunda REST API specification, optimized for the documentation.
+
+`,
+  };
+}
+
+function redefineCreateProcessInstanceRequest(originalSpec) {
+  // Redefines the CreateProcessInstanceRequest schema to describe a union of two possible request bodies.
+  //   This union type does not work upstream, but we can rewrite it here to more clearly describe the schema.
+
+  if (originalSpec.includes("CreateProcessInstanceRequestBase")) {
+    // Make this a repeatable task by checking if it's run already.
+    console.log("skipping redefineCreateProcessInstanceRequest...");
+    return [];
+  }
+
+  // Diff created by these changes:
+  //   CreateProcessInstanceRequest:
+  //     type: object
+  //+    oneOf:
+  //+      - $ref: "#/components/schemas/CreateProcessInstanceRequestByKey"
+  //+      - $ref: "#/components/schemas/CreateProcessInstanceRequestById"
+  //+   CreateProcessInstanceRequestByKey:
+  //+    type: object
+  //+    allOf:
+  //+      - $ref: "#/components/schemas/CreateProcessInstanceRequestBase"
+  //     properties:
+  //       processDefinitionKey:
+  //         ...
+  //+  CreateProcessInstanceRequestById:
+  //+    type: object
+  //+    allOf:
+  //+      - $ref: "#/components/schemas/CreateProcessInstanceRequestBase"
+  //+    properties:
+  //       processDefinitionId:
+  //         ...
+  //       processDefinitionVersion:
+  //         ...
+  //+  CreateProcessInstanceRequestBase:
+  //+    type: object
+  //+    properties:
+  //       variables:
+  //         ...
+  //       tenantId:
+  //         ...
+  //       ...
+
+  return [
+    // 1. Convert the main request to a oneOf union and define the first possible type.
+    {
+      // match the start of the CreateProcessInstanceRequest object
+      from: /    CreateProcessInstanceRequest:\n      type: object/m,
+
+      // append the `oneOf` declaration to define the union type.
+      //   Then a definition for the first possible type, so that it includes the `processDefinitionKey` property.
+      to: `    CreateProcessInstanceRequest:
+      type: object
+      oneOf:
+        - $ref: "#/components/schemas/CreateProcessInstanceRequestByKey"
+        - $ref: "#/components/schemas/CreateProcessInstanceRequestById"
+    CreateProcessInstanceRequestByKey:
+      type: object
+      allOf:
+        - $ref: "#/components/schemas/CreateProcessInstanceRequestBase"`,
+    },
+
+    // 2. Define the second possible type, to include the `processDefinitionId` property.
+    {
+      // match the start of the CreateProcessInstanceRequestByKey object, up until the `processDefinitionId` property (non-inclusive).
+      from: /    CreateProcessInstanceRequestByKey:[\s\S]*?(?=\s*processDefinitionId:)/m,
+
+      // append the second possible type definition, so that it includes the `processDefinitionId`.
+      to: `$&
+    CreateProcessInstanceRequestById:
+      type: object
+      allOf:
+        - $ref: "#/components/schemas/CreateProcessInstanceRequestBase"
+      properties:`,
+    },
+
+    // 3. Define a base type to contain the common properties, starting before the `variables` property.
+    {
+      // match the start of the CreateProcessInstanceRequestById object, up until the `variables` property (non-inclusive).
+      from: /    CreateProcessInstanceRequestById:[\s\S]*?(?=\s*variables:)/m,
+      // append the base type definition, so that it includes all remaining properties.
+      to: `$&
+    CreateProcessInstanceRequestBase:
+      type: object
+      properties:`,
+    },
+  ];
+}
+
+function redefineEvaluateDecisionRequest(originalSpec) {
+  // Redefines the EvaluateDecisionRequest schema to describe a union of two possible request bodies.
+  //   This union type does not work upstream, but we can rewrite it here to more clearly describe the schema.
+
+  if (originalSpec.includes("EvaluateDecisionRequestBase")) {
+    // Make this a repeatable task by checking if it's run already.
+    console.log("skipping redefineEvaluateDecisionRequest...");
+    return [];
+  }
+
+  // Diff created by these changes:
+  //   EvaluateDecisionRequest:
+  //     type: object
+  //+    oneOf:
+  //+      - $ref: "#/components/schemas/EvaluateDecisionRequestByKey"
+  //+      - $ref: "#/components/schemas/EvaluateDecisionRequestById"
+  //+  EvaluateDecisionRequestByKey:
+  //+    type: object
+  //+    allOf:
+  //+      - $ref: "#/components/schemas/EvaluateDecisionRequestBase"
+  //     properties:
+  //       decisionDefinitionKey:
+  //         ...
+  //+  EvaluateDecisionRequestById:
+  //+    type: object
+  //+    allOf:
+  //+      - $ref: "#/components/schemas/EvaluateDecisionRequestBase"
+  //+    properties:
+  //       decisionDefinitionId:
+  //         ...
+  //+  EvaluateDecisionRequestBase:
+  //+    type: object
+  //+    properties:
+  //       variables:
+  //         ...
+  //       tenantId:
+  //         ...
+  //       ...
+
+  return [
+    // 1. Convert the main request to a oneOf union and define the first possible type.
+    {
+      // match the start of the EvaluateDecisionRequest object
+      from: /    EvaluateDecisionRequest:\n      type: object/m,
+
+      // append the `oneOf` declaration to define the union type.
+      //   Then a definition for the first possible type, so that it includes the `decisionDefinitionKey` property.
+      to: `    EvaluateDecisionRequest:
+      type: object
+      oneOf:
+        - $ref: "#/components/schemas/EvaluateDecisionRequestByKey"
+        - $ref: "#/components/schemas/EvaluateDecisionRequestById"
+    EvaluateDecisionRequestByKey:
+      type: object
+      allOf:
+        - $ref: "#/components/schemas/EvaluateDecisionRequestBase"`,
+    },
+
+    // 2. Define the second possible type, to include the `decisionDefinitionId` property.
+    {
+      // match the start of the EvaluateDecisionRequestByKey object, up until the `decisionDefinitionId` property (non-inclusive).
+      from: /    EvaluateDecisionRequestByKey:[\s\S]*?(?=\s*decisionDefinitionId:)/m,
+
+      // append the second possible type definition, so that it includes the `decisionDefinitionId`.
+      to: `$&
+    EvaluateDecisionRequestById:
+      type: object
+      allOf:
+        - $ref: "#/components/schemas/EvaluateDecisionRequestBase"
+      properties:`,
+    },
+
+    // 3. Define a base type to contain the common properties, starting before the `variables` property.
+    {
+      // match the start of the CreateProcessInstanceRequestById object, up until the `variables` property (non-inclusive).
+      from: /    EvaluateDecisionRequestById:[\s\S]*?(?=\s*variables:)/m,
+      // append the base type definition, so that it includes all remaining properties.
+      to: `$&
+    EvaluateDecisionRequestBase:
+      type: object
+      properties:`,
+    },
+  ];
 }
 
 module.exports = {
@@ -16,33 +213,3 @@ module.exports = {
   preGenerateDocs,
   postGenerateDocs,
 };
-
-function hackChangesetDescription() {
-  // This is a temporary hack, until https://github.com/camunda/camunda-docs/issues/3568 is resolved.
-  //   The OpenAPI generator plugin we're using does not use the correct `description` property
-  //   for the `UserTaskUpdateRequest` object. Instead of picking up the actual property description,
-  //   it picks up the description of the first merged schema in the `allOf` property (i.e. from the `Changeset` schema).
-  // This adjustment replaces the description of the `Changeset` schema with the current description of
-  //   the `UserTaskUpdateRequest.changeset` property.
-  console.log("hacking changeset description...");
-  replace.sync({
-    files: `${specFile}`,
-    from: /^      description: A map of changes.$/m,
-    to: `      description: |
-        JSON object with changed task attribute values.
-
-        The following attributes can be adjusted with this endpoint, additional attributes
-        will be ignored:
-
-        * \`candidateGroups\` - reset by providing an empty list
-        * \`candidateUsers\` - reset by providing an empty list
-        * \`dueDate\` - reset by providing an empty String
-        * \`followUpDate\` - reset by providing an empty String
-
-        Providing any of those attributes with a \`null\` value or omitting it preserves
-        the persisted attribute's value.
-
-        The assignee cannot be adjusted with this endpoint, use the Assign task endpoint.
-        This ensures correct event emission for assignee changes.`,
-  });
-}
