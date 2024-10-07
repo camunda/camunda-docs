9cbcc0adc5bccc2e66429e03415aa590746bd463
Christina Ausley <christina.ausley@camunda.com>
2024-10-07 12:36:38 -0400

    adjust sidebar

--- sidebars.js
+++ sidebars.js
@@ -328,7 +328,6 @@ module.exports = {
                 "components/connectors/out-of-the-box-connectors/amazon-sns",
                 "components/connectors/out-of-the-box-connectors/amazon-sqs",
                 "components/connectors/out-of-the-box-connectors/amazon-textract",
-                "components/connectors/out-of-the-box-connectors/amazon-comprehend",
               ],
             },
             "components/connectors/out-of-the-box-connectors/blueprism",
