5164cd06414bb2b69edc4b435bc4a08a58088c0f
Langleu <lars.lange@camunda.com>
2024-11-18 14:34:18 +0100

    chore(self-managed): add generalized reference architecture concept page

--- sidebars.js
+++ sidebars.js
@@ -1079,6 +1079,7 @@ module.exports = {
         "self-managed/concepts/mapping-rules",
         "self-managed/concepts/elasticsearch-privileges",
         "self-managed/concepts/opensearch-privileges",
+        "self-managed/concepts/reference-architecture",
       ],
     },
     {
