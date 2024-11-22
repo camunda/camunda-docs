92d88b12dd689bd52688a97af269e1761888c7cb
Amara <amara.graham@camunda.com>
2024-11-22 14:52:51 -0600

    remove reference & sidebar

--- sidebars.js
+++ sidebars.js
@@ -805,7 +805,6 @@ module.exports = {
         require("./docs/apis-tools/zeebe-api/sidebar-schema"),
         {
           Deprecated: [
-            require("./docs/apis-tools/tasklist-api/sidebar-schema"),
             require("./docs/apis-tools/zeebe-api-rest/sidebar-schema"),
           ],
         },
