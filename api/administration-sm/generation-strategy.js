1fa4de071a80b3057068af119c4bedc337fdfd69
Steven Hicks <steven.hicks@camunda.com>
2024-12-17 15:50:59 -0600

    fix: move infrastructure for administration sm api, from console sm

--- api/administration-sm/generation-strategy.js
+++ api/administration-sm/generation-strategy.js
@@ -1,8 +1,8 @@
 const { makeServerDynamic } = require("../make-server-dynamic");
 const removeDuplicateVersionBadge = require("../remove-duplicate-version-badge");
 
-const outputDir = "docs/apis-tools/console-sm-api/specifications";
-const specFile = "api/console-sm/console-sm-openapi.yaml";
+const outputDir = "docs/apis-tools/administration-sm-api/specifications";
+const specFile = "api/administration-sm/administration-sm-openapi.yaml";
 
 function preGenerateDocs() {
   makeServerDynamic(specFile);
