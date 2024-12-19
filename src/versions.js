fa3bd92f9fd61ce170d44889ef234e12020ae7d3
Steven Hicks <steven.hicks@camunda.com>
2024-12-19 12:21:56 -0600

    temp: remove old versions

--- src/versions.js
+++ src/versions.js
@@ -19,18 +19,18 @@ const versionMappings = [
     docsVersion: "8.6",
     optimizeVersion: "3.14.0",
   },
-  {
-    docsVersion: "8.5",
-    optimizeVersion: "3.13.0",
-  },
-  {
-    docsVersion: "8.4",
-    optimizeVersion: "3.12.0",
-  },
-  {
-    docsVersion: "8.3",
-    optimizeVersion: "3.11.0",
-  },
+  // {
+  //   docsVersion: "8.5",
+  //   optimizeVersion: "3.13.0",
+  // },
+  // {
+  //   docsVersion: "8.4",
+  //   optimizeVersion: "3.12.0",
+  // },
+  // {
+  //   docsVersion: "8.3",
+  //   optimizeVersion: "3.11.0",
+  // },
 ];
 
 /** @type {Array<UnsupportedVersion>} */
