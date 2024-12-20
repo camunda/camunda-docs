4dca1abc3578b3eef22d985927b15087b397a332
Steven Hicks <steven.hicks@camunda.com>
2024-12-20 14:47:59 -0600

    feat: configure 8.6 tasklist api generation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,6 +108,18 @@ module.exports = {
               groupPathsBy: "tag",
             },
             hideSendButton: true,
+            version: "1",
+            label: "Unused but required field",
+            baseUrl: "Unused but required field",
+            versions: {
+              8.6: {
+                specPath: "api/tasklist/version-8.6/tasklist-openapi.yaml",
+                outputDir:
+                  "versioned_docs/version-8.6/apis-tools/tasklist-api-rest/specifications",
+                label: "Unused but required field",
+                baseUrl: "Unused but required field",
+              },
+            },
           },
         },
       },
