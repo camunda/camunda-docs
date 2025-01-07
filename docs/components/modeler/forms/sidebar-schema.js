272962b06773159d20592d1039cca77b8e1b3182
Vinícius Goulart <vinicius.goulart@camunda.com>
2025-01-07 14:16:27 +0100

    feat: add form-js document preview component docs (#4773)

--- docs/components/modeler/forms/sidebar-schema.js
+++ docs/components/modeler/forms/sidebar-schema.js
@@ -25,6 +25,7 @@ module.exports = {
         lib_dir + "forms-element-library-table",
         lib_dir + "forms-element-library-expression",
         lib_dir + "forms-element-library-filepicker",
+        lib_dir + "forms-element-library-document-preview",
         lib_dir + "forms-element-library-image",
         lib_dir + "forms-element-library-spacer",
         lib_dir + "forms-element-library-separator",
