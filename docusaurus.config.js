fbcdb3d8d2443166b4c2fdfd1e45ea79b1d4b8bb
Steven Hicks <steven.hicks@camunda.com>
2025-01-17 15:30:44 -0600

    d3: configure 8.5 operate api docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,6 +89,13 @@ module.exports = {
                 label: "Unused but required field",
                 baseUrl: "Unused but required field",
               },
+              8.5: {
+                specPath: "api/operate/version-8.5/operate-openapi.yaml",
+                outputDir:
+                  "versioned_docs/version-8.5/apis-tools/operate-api/specifications",
+                label: "Unused but required field",
+                baseUrl: "Unused but required field",
+              },
             },
           },
         },
