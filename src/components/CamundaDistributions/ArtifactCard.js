51780709aa0495bfc831a46d23e4a2a942d92ecf
Ahmed AbouZaid <6760103+aabouzaid@users.noreply.github.com>
2025-01-30 12:30:14 +0100

    tidy up

--- src/components/CamundaDistributions/ArtifactCard.js
+++ src/components/CamundaDistributions/ArtifactCard.js
@@ -1,77 +1,159 @@
+// import React, { useState } from "react";
+// import Link from "@docusaurus/Link";
+// import CodeBlock from "@theme/CodeBlock";
+// import styles from "./styles.module.css";
+
+// const ArtifactCard = (artifactData = Object) => {
+//   const versions = artifactData.keys()
+//   return (
+//     <div className={styles.card}>
+//       <div className={styles.dropdownContainer}>
+//       {version != null && (
+//           <>
+//             <label>Version</label>
+//             <select
+//               value={version}
+//               onChange={(e) => setVersion(e.target.value)}
+//             >
+//               {versions.map((version) => (
+//                 <option key={version} value={version}>
+//                   {version}
+//                 </option>
+//               ))}
+//             </select>
+//           </>
+//         )}
+//         {artifactData.system != "" && (
+//           <>
+//             <label>System</label>
+//             <select
+//               value={artifactData.system}
+//               onChange={(e) => setArchitecture(e.target.value)}
+//             >
+//               {artifactData.system.map((os) => (
+//                 <option key={os} value={os}>
+//                   {os}
+//                 </option>
+//               ))}
+//             </select>
+//           </>
+//         )}
+//       </div>
+//       <div>
+//         {downloadURL != "" && (
+//           <Link
+//             to={downloadURL}
+//             title={downloadURL}
+//             className={styles.downloadURL}
+//           >
+//             Download
+//           </Link>
+//         )}
+//         <br />
+//         {runCommand != "" && (
+//           <CodeBlock language="shell">
+//             {runCommand.replaceAll("${version}", version)}
+//           </CodeBlock>
+//         )}
+//       </div>
+//       <div className={styles.linkContainer}>
+//       {extraLinks.forEach(element => {
+//           <Link to={element.url}>{element.desc}</Link>
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ArtifactCard;
 import React, { useState } from "react";
+import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
 import CodeBlock from "@theme/CodeBlock";
-import styles from "./styles.module.css";
 
-const ArtifactCard = ({
-  osName = "",
-  architectures = [],
-  versions = [],
-  downloadURL = "",
-  releaseNotesUrl = "",
-  runCommand = "",
-}) => {
-  const [architecture, setArchitecture] = useState(architectures[0]);
-  const [version, setVersion] = useState(versions[0]);
+const ArtifactCard = ({ data, runCommand = "" }) => {
+  const versions = Object.keys(data);
 
-  const handleDownload = (downloadURL) => {
-    window.open(downloadURL, "_blank");
+  // Ensure at least one version exists
+  const [selectedVersion, setSelectedVersion] = useState(versions[0] || "");
+  const [selectedSystem, setSelectedSystem] = useState(
+    data[versions[0]]?.[0]?.system || ""
+  );
+
+  const handleVersionChange = (event) => {
+    const newVersion = event.target.value;
+    setSelectedVersion(newVersion);
+    setSelectedSystem(data[newVersion]?.[0]?.system || "");
   };
+
+  const handleSystemChange = (event) => {
+    setSelectedSystem(event.target.value);
+  };
+
+  const selectedData =
+    data[selectedVersion]?.find((item) => item.system === selectedSystem) || {};
 
   return (
     <div className={styles.card}>
       <div className={styles.dropdownContainer}>
-        {osName != "" && (
-          <>
-            <label>System</label>
-            <select
-              value={architecture}
-              onChange={(e) => setArchitecture(e.target.value)}
-            >
-              {architectures.map((arch) => (
-                <option key={arch} value={arch}>
-                  {arch}
+        <label>
+          Version:
+          <select value={selectedVersion} onChange={handleVersionChange}>
+            {versions.map((version) => (
+              <option key={version} value={version}>
+                {version}
+              </option>
+            ))}
+          </select>
+        </label>
+        {selectedSystem && (
+          <label>
+            System:
+            <select value={selectedSystem} onChange={handleSystemChange}>
+              {data[selectedVersion]?.map((item) => (
+                <option key={item.system} value={item.system}>
+                  {item.system}
                 </option>
               ))}
             </select>
-          </>
-        )}
-        {version != null && (
-          <>
-            <label>Version</label>
-            <select
-              value={version}
-              onChange={(e) => setVersion(e.target.value)}
-            >
-              {versions.map((ver) => (
-                <option key={ver} value={ver}>
-                  {ver}
-                </option>
-              ))}
-            </select>
-          </>
+          </label>
         )}
       </div>
-      <div>
-        {downloadURL != "" && (
+
+      {selectedData.download && (
+        <>
+          <h4>Get</h4>
           <Link
-            to={downloadURL}
-            title={downloadURL}
+            to={selectedData.download}
+            title={selectedData.download}
             className={styles.downloadURL}
           >
             Download
           </Link>
-        )}
-        <br />
-        {runCommand != "" && (
-          <CodeBlock language="shell">
-            {runCommand.replaceAll("${version}", version)}
-          </CodeBlock>
-        )}
-      </div>
-      <div className={styles.linkContainer}>
-        <Link to={releaseNotesUrl}>View Release Notes</Link>
-      </div>
+        </>
+      )}
+
+      <h4>Run</h4>
+      {runCommand && (
+        <CodeBlock language="shell">
+          {runCommand.replace("${version}", selectedVersion)}
+        </CodeBlock>
+      )}
+
+      {selectedData.links && (
+        <>
+          <h4>Links</h4>
+          <ul>
+            {selectedData.links.map((link, index) => (
+              <li key={index}>
+                <a href={link.link} target="_blank" rel="noopener noreferrer">
+                  {link.description}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
--- src/components/CamundaDistributions/DockerCompose.js
+++ src/components/CamundaDistributions/DockerCompose.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { useActiveVersion } from "@docusaurus/plugin-content-docs/client";
+import { getDocsVersion } from "./utilz";
 
 const DockerComposeBaseURL =
   "https://github.com/camunda/camunda-self-managed/releases/download";
 
-const getVersion = () => {
-  const docsVersion = useActiveVersion();
-  if (docsVersion.label == "Next") return "alpha";
-  // NOTE: This is a workaround for the irregular release cut of the 8.7 version.
-  // TODO: Remove this condition once the 8.7 is released.
-  if (docsVersion.label == "8.7") return "alpha";
-  return docsVersion.label;
-};
-
 export const DockerComposeURL = () => {
-  const version = getVersion();
+  const version = getDocsVersion();
   return `${DockerComposeBaseURL}/docker-compose-${version}/docker-compose-${version}.zip`;
 };
 
