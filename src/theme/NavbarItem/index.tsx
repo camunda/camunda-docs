a5c93325c08a8cb6faeed680b2276e9f1e56d16d
Steven Hicks <steven.hicks@camunda.com>
2025-02-07 14:52:53 -0600

    remove unneeded code

--- src/theme/NavbarItem/index.tsx
+++ src/theme/NavbarItem/index.tsx
@@ -1,7 +1,6 @@
 // Why is this swizzled?
 //   1. To render the correct versions in the version dropdown.
 //     If they're viewing optimize docs, we pass the optimize docs plugin ID to the version dropdown.
-//   2. To override the label for "next" versions, which is not configurable in docusaurus.config.js.
 // Swizzled from version 3.7.0.
 
 import React, { type ReactNode } from "react";
@@ -10,10 +9,8 @@ import type NavbarItemType from "@theme/NavbarItem";
 import type { WrapperProps } from "@docusaurus/types";
 
 import { useLocation } from "@docusaurus/router";
-import { useLabelOverrides } from "./useLabelOverrides";
 
 type Props = WrapperProps<typeof NavbarItemType> & {
-  label: string;
   docsPluginId: string;
 };
 
@@ -29,9 +26,6 @@ export default function NavbarItemWrapper(props: Props): ReactNode {
       childProps.docsPluginId = "optimize";
     }
   }
-
-  // override label for "next" versions
-  childProps.label = useLabelOverrides(props.label);
 
   return (
     <>
