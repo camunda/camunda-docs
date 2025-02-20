4889c6bf17ca129b8491d6dce63a28940be4441f
Mark Sellings <mark.sellings@camunda.com>
2025-02-20 18:06:06 +0000

    Backport migration overhaul documentation (#5045)
    * Backport to 8.7
    
    * Backport to 8.6
    
    * Add redirects

---
+++ versioned_docs/version-8.6/guides/react-components/_migration-card-data.js
@@ -0,0 +1,48 @@
+import IconPrepImg from "../img/icon-migration-checklist.png";
+import IconCodeImg from "../img/icon-migration-code.png";
+import IconToolingImg from "../img/icon-migration-tooling.png";
+import IconBpmImg from "../img/icon-migration-bpm.png";
+import IconJourneyImg from "../img/icon-migration-journey.png";
+import IconConceptImg from "../img/icon-migration-concept.png";
+
+export const gettingStartedCards = [
+  {
+    link: "./migration-journey",
+    title: "Migration journey",
+    image: IconJourneyImg,
+    description: "Learn about common themes in the migration journey.",
+  },
+  {
+    link: "./conceptual-differences",
+    title: "Conceptual differences",
+    image: IconConceptImg,
+    description:
+      "Important conceptual differences to know when planning your migration.",
+  },
+  {
+    link: "./migration-tooling",
+    title: "Migration tooling",
+    image: IconToolingImg,
+    description:
+      "Camunda tooling to ease migration from Camunda 7 to Camunda 8.",
+  },
+  {
+    link: "./code-conversion",
+    title: "Code conversion",
+    image: IconCodeImg,
+    description: "Help with converting your code for migration.",
+  },
+  {
+    link: "./migration-readiness",
+    title: "Migration preparation",
+    image: IconPrepImg,
+    description:
+      "Rules and development practices for smooth migration preparation.",
+  },
+  {
+    link: "./technical-details",
+    title: "Technical details",
+    image: IconBpmImg,
+    description: "Technical details around BPMN and DMN model differences.",
+  },
+];
