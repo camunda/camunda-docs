c10a761fea8b4dbdb8c370516ce8f5d248cb1f70
Christina Ausley <christina.ausley@camunda.com>
2025-03-04 15:19:27 -0500

    zeebe landing page rework

---
+++ docs/components/zeebe/react-components/_zeebe-card-data.js
@@ -0,0 +1,11 @@
+// import IconPlayImg from "../img/icon-play.png";
+
+// Getting started cards on Zeebe introduction page
+export const gettingStartedCards = [
+  {
+    link: "../zeebe/technical-concepts/clustering",
+    title: "Clustering",
+    // image: IconPlayImg,
+    description: "lorem ipsum",
+  },
+];
