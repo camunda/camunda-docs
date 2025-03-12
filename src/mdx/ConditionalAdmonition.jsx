4232b5971abce9bb73181b57189acf7f84f5583f
Langleu <lars.lange@camunda.com>
2025-03-12 14:42:03 +0100

    feat: add date conditional admonition

---
+++ src/mdx/ConditionalAdmonition.jsx
@@ -0,0 +1,24 @@
+import Admonition from '@theme/Admonition';
+import Markdown from 'react-markdown';
+
+// The purpose of this component is to conditionally render an admonition based on a date.
+// The date is a string in the format of "YYYY-MM-DD".
+// Example usage:
+// <ConditionalAdmonition type="danger" date="2025-04-01" text="This is an important message." />
+// Useful for example during a release period where you want to implement the message already and not have to wait for the release branch.
+// The text can be markdown formatted.
+
+const ConditionalAdmonition = ({ date, text, ...props }) => {
+  const currentDate = new Date();
+  const targetDate = new Date(date);
+
+  return currentDate >= targetDate ? 
+
+    <Admonition {...props}>
+      <Markdown>{text}</Markdown>
+    </Admonition>
+
+    : null;
+};
+
+export default ConditionalAdmonition;
