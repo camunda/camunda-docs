b0e42930ca8d1a082a797133facfd344c2e23450
Steven Hicks <steven.hicks@camunda.com>
2025-03-19 13:55:23 -0500

    de-optimize(8.3): remove 3.11.0 from manifests

--- src/versions.js
+++ src/versions.js
@@ -32,10 +32,6 @@ const versionMappings = [
     docsVersion: "8.4",
     optimizeVersion: "3.12.0",
   },
-  {
-    docsVersion: "8.3",
-    optimizeVersion: "3.11.0",
-  },
 ];
 
 /** @type {Array<UnsupportedVersion>} */
