d42ca787d72740fc2c905c88f148fc8392c47fd7
Langleu <lars.lange@camunda.com>
2025-03-20 13:42:15 +0100

    chore: fix conditionaladminiton link

--- src/mdx/ConditionalAdmonition.jsx
+++ src/mdx/ConditionalAdmonition.jsx
@@ -7,6 +7,7 @@ import Markdown from "react-markdown";
 // <ConditionalAdmonition type="danger" date="2025-04-01" text="This is an important message." />
 // Useful for example during a release period where you want to implement the message already and not have to wait for the release branch.
 // The text can be markdown formatted.
+// Markdown links need to be static and don't adhere to the normal MDX link resolution.
 
 const ConditionalAdmonition = ({ date, text, ...props }) => {
   const currentDate = new Date();
