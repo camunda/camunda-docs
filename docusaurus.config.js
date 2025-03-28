869761d40244324414cd9bae69fe92d91352ebfd
Steven Hicks <steven.j.hicks@gmail.com>
2025-03-28 13:48:58 -0500

    chore: optimize reabsorption (v8.4) (#5370)
    * de-optimize(8.4): move content back into main instance
    
    * chore: optimize reabsorption (v8.4) -- reviewable PR (#5371)
    
    * de-optimize(8.4): move items from optimize sidebars to main sidebars
    
    * de-optimize(8.4): remove 3.12.0 from manifests
    
    * de-optimize(8.4): update links
    
    * de-optimize(8.4): update cross-instance links
    
    * de-optimize(8.4): update card links
    
    * de-optimize(8.4): redirect rules

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,10 +65,6 @@ module.exports = {
             label: "8.5 / 3.13.0",
             banner: "none",
           },
-          "3.12.0": {
-            label: "8.4 / 3.12.0",
-            banner: "none",
-          },
           "3.11.0": {
             label: "8.3 / 3.11.0",
             banner: "none",
@@ -592,7 +588,6 @@ module.exports = {
             "/docs/8.4/**",
             "/docs/8.5/**",
             "/optimize/3.11.0/**",
-            "/optimize/3.12.0/**",
             "/optimize/3.13.0/**",
             "/optimize/3.15.0/**",
             "/optimize/next/**",
