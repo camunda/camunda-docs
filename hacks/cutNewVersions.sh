61554c71b159d18cfce733d8e7351a27c4a8b44a
Steven Hicks <steven.j.hicks@gmail.com>
2025-04-24 10:11:36 -0500

    versioning: update scripts and docs (#5619)

--- hacks/cutNewVersions.sh
+++ hacks/cutNewVersions.sh
@@ -2,24 +2,12 @@
 set -e
 
 # Before running this script make sure these versions are correct!
-NEW_DOCS_VERSION="8.6"
-PREVIOUS_DOCS_VERSION="8.5"
-NEW_OPTIMIZE_VERSION="3.14.0"
-PREVIOUS_OPTIMIZE_VERSION="3.13.0"
+NEW_DOCS_VERSION="8.8"
+PREVIOUS_DOCS_VERSION="8.7"
+NEW_NEXT_DOCS_VERSION="8.9"
 
-echo "1/6 Creating version $NEW_DOCS_VERSION of main docs..."
+echo "1/4 Creating version $NEW_DOCS_VERSION of main docs..."
 npm run docusaurus docs:version $NEW_DOCS_VERSION
-
-echo "2/6 Creating version $NEW_OPTIMIZE_VERSION of Optimize docs..."
-npm run docusaurus docs:version:optimize $NEW_OPTIMIZE_VERSION
-
-echo "3/6 Correcting sidebars files of new versions..."
-sed -i '' 's/\"href\": \"\/optimize\/next\//\"href\": \"\/optimize\//g' versioned_sidebars/version-$NEW_DOCS_VERSION-sidebars.json
-sed -i '' 's/\"href\": \"\/docs\/next\//\"href\": \"\/docs\//g' optimize_versioned_sidebars/version-$NEW_OPTIMIZE_VERSION-sidebars.json
-
-echo "4/6 Correcting sidebars files of previous versions..."
-sed -i '' "s/\"href\": \"\/optimize\//\"href\": \"\/optimize\/${PREVIOUS_OPTIMIZE_VERSION}\//g" versioned_sidebars/version-$PREVIOUS_DOCS_VERSION-sidebars.json
-sed -i '' "s/\"href\": \"\/docs\//\"href\": \"\/docs\/${PREVIOUS_DOCS_VERSION}\//g" optimize_versioned_sidebars/version-$PREVIOUS_OPTIMIZE_VERSION-sidebars.json
 
 # commit the big stuff first, so that we can split into two PRs -- a big one, and a reviewable one.
 git add versioned_docs versioned_sidebars optimize_versioned_docs optimize_versioned_sidebars
@@ -32,19 +20,25 @@ if [ -n "$(git status --porcelain ./hacks/cutNewVersions.sh)" ]; then
   git commit -m "chore: update versions in cutNewVersions.sh"
 fi
 
-git add versions.json optimize_versions.json
-git commit -m "release: new version numbers"
+git add versions.json
+git commit -m "release: new version number for versions.json"
 
-echo "5/6 Correcting current-version redirects..."
+echo "2/4 Correcting versions.js..."
+sed -i '' "s/${PREVIOUS_DOCS_VERSION}/${NEW_DOCS_VERSION}/" ./src/versions.js
+
+git add ./src/versions.js
+git commit -m "release: new version number for src/versions.js"
+
+echo "3/4 Correcting current-version redirects..."
+sed -i '' "s/docs\/${NEW_DOCS_VERSION}\/(\.\*)/docs\/${NEW_NEXT_DOCS_VERSION}\/(\.\*)/" ./static/.htaccess
 sed -i '' "s/docs\/${PREVIOUS_DOCS_VERSION}\/(\.\*)/docs\/${NEW_DOCS_VERSION}\/(\.\*)/" ./static/.htaccess
-sed -i '' "s/optimize\/${PREVIOUS_OPTIMIZE_VERSION}\/(\.\*)/optimize\/${NEW_OPTIMIZE_VERSION}\/(\.\*)/" ./static/.htaccess
 
 git add ./static/.htaccess
 git commit -m "release: new version numbers for content redirects"
 
-echo "6/6 Correcting check-versions workflow configuration..."
+echo "4/4 Correcting check-versions workflow configuration..."
+sed -i '' "s/${NEW_DOCS_VERSION}/${NEW_NEXT_DOCS_VERSION}/" ./.github/workflows/check-versions/version-config.json
 sed -i '' "s/${PREVIOUS_DOCS_VERSION}/${NEW_DOCS_VERSION}/" ./.github/workflows/check-versions/version-config.json
-sed -i '' "s/${PREVIOUS_OPTIMIZE_VERSION}/${NEW_OPTIMIZE_VERSION}/" ./.github/workflows/check-versions/version-config.json
 
 git add ./.github/workflows/check-versions/version-config.json
 git commit -m "release: new version numbers in check-versions workflow"
