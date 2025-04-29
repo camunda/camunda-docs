d6bad26c32951113b1bf743b56c9dc767d64b5c7
Adam <adam.urban@gmail.com>
2025-04-29 09:05:22 +0200

    feat(console): add section about usage metrics

--- sidebars.js
+++ sidebars.js
@@ -1207,6 +1207,7 @@ module.exports = {
                 "self-managed/console-deployment/configuration/ssl",
               ],
             },
+            "self-managed/console-deployment/usage-metrics",
             "self-managed/console-deployment/telemetry",
           ],
           Zeebe: [
