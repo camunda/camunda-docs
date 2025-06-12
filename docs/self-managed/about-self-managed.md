f9812010e40c6ad2838fbbc2f936cdc5e2a20f59
mesellings <mark.sellings@camunda.com>
2025-06-12 17:30:53 +0100

    Additional refinement to self-managed landing page

--- docs/self-managed/about-self-managed.md
+++ docs/self-managed/about-self-managed.md
@@ -7,21 +7,20 @@ description: "Step through everything you need to download, configure, and work 
 import Components from './react-components/components.md'
 import OverviewImg from './assets/self-managed-hero.png';
 import CardGrid from './react-components/\_card';
-import { gettingStartedCards, installCards } from './react-components/\_card-data';
+import { gettingStartedCards, installCards, referenceCards } from './react-components/\_card-data';
+import DocCardList from '@theme/DocCardList';
 
-<h3 style={{marginTop: '-10px', marginBottom: '60px', fontWeight: 'normal'}}>Get started with our self-hosted alternative to Camunda 8 SaaS.</h3>
+<h3 style={{marginTop: '-10px', marginBottom: '60px', fontWeight: 'normal'}}>Get started with the self-hosted alternative to Camunda 8 SaaS.</h3>
 
 <div class="double-column-container">
 <div class="double-column-left"  style={{marginRight: '50px', flex: '1.35'}}>
 
 Building process automation solutions with Camunda 8 works similarly regardless of hosting and deployment. Learn how to download, configure, and use Self-Managed components and features.
 
-<a class="button button--outline button--secondary button--md button--hero--topic" title="Get started with Self-Managed" href="../run-locally" style={{marginBottom: '30px', marginTop: '20px'}}>Install and run Self-Managed locally</a>
+<a class="button button--outline button--secondary button--md button--hero--topic" title="Get started with Self-Managed" href="../run-locally" style={{marginBottom: '30px', marginTop: '20px'}}>Install and run Camunda 8 locally</a>
 
 </div>
 <div class="double-column-right" style={{flex: '1'}}>
-
-For more information on Camunda 8 SaaS, visit [What is Camunda 8?](/components/components-overview.md) If you are new to Camunda 8, we recommend you start your journey with [Camunda 8 SaaS-based guides](../../guides/).
 
 <img src={OverviewImg} alt="Image showing Self-Managed components and features" title="Use Camunda 8 Self-Managed as a self-hosted alternative to Camunda 8 SaaS" class="img-noborder img-600 img-transparent hero-topic fade-in-top-image" style={{marginTop: '0', marginBottom: '0'}}/>
 
@@ -30,7 +29,7 @@ For more information on Camunda 8 SaaS, visit [What is Camunda 8?](/components/c
 
 ## Install and run locally
 
-Install and run Camunda 8 Self-Managed locally using Camunda 8 Run, Docker Compose, or your local Kubernetes cluster.
+New to Camunda 8 Self-Managed? Get started by installing and running Camunda 8 Self-Managed locally.
 
 <CardGrid card={gettingStartedCards} />
 
@@ -39,6 +38,12 @@ Install and run Camunda 8 Self-Managed locally using Camunda 8 Run, Docker Compo
 Install and run Camunda 8 Self-Managed in production using Helm, Docker, or a manual setup.
 
 <CardGrid card={installCards} />
+
+## Reference architectures
+
+Guidance for enterprise architects, developers, and IT managers wanting to streamline deployments and improve system reliability. Reference architectures provide comprehensive blueprints for designing and implementing scalable, robust, and adaptable systems.
+
+<CardGrid card={referenceCards} />
 
 ## Configuration guides
 
@@ -93,3 +98,7 @@ Learn more about different use cases and scenarios for configuring Camunda 8 bey
 ## Concepts
 
 ## Components
+
+:::info
+For more information on Camunda 8 SaaS, visit [What is Camunda 8?](/components/components-overview.md) If you are new to Camunda 8, we recommend you start your journey with [Camunda 8 SaaS-based guides](../../guides/).
+:::
--- docs/self-managed/react-components/_card-data.js
+++ docs/self-managed/react-components/_card-data.js
@@ -4,6 +4,8 @@ import IconUpdate from "../assets/icon-sm-update.png";
 import IconC8Run from "../assets/icon-sm-c8run.png";
 import IconDocker from "../assets/icon-sm-docker.png";
 import IconKubernetes from "../assets/icon-sm-kubernetes.png";
+import IconManual from "../assets/icon-sm-manual.png";
+import IconJar from "../assets/icon-sm-jar.png";
 
 // Getting started cards on introduction page
 export const gettingStartedCards = [
@@ -11,46 +13,80 @@ export const gettingStartedCards = [
     link: "../setup/deploy/local/c8run",
     title: "Camunda 8 Run",
     image: IconC8Run,
-    description:
-      "Install and run a simplified, single-application Camunda configuration.",
+    description: "Install and run our simplified, single-application Camunda 8 configuration.",
   },
   {
     link: "../setup/deploy/local/docker-compose",
     title: "Docker Compose",
     image: IconDocker,
-    description:
-      "Use a Docker Compose configuration to run Camunda Self-Managed.",
+    description: "Use a Docker Compose configuration to run Camunda Self-Managed.",
   },
   {
     link: "../setup/deploy/local/local-kubernetes-cluster",
     title: "Local Kubernetes cluster",
     image: IconKubernetes,
-    description:
-      "Deploy Camunda 8 Self-Managed on your Kubernetes local cluster.",
+    description: "Deploy Camunda 8 Self-Managed on your local Kubernetes cluster.",
   },
 ];
 
 // Installation cards on introduction page
 export const installCards = [
   {
-    link: "../setup/overview",
-    title: "Install",
-    image: IconConfig,
-    description:
-      "Install Camunda 8 Self-Managed using Helm, Docker, or manually.",
+    link: "../setup/install",
+    title: "Helm",
+    image: IconKubernetes,
+    description: "Use Kubernetes and Helm to deploy and run Camunda 8 Self-Managed.",
   },
   {
-    link: "../reference-architecture",
-    title: "Reference architectures",
-    image: IconRefArch,
-    description:
-      "Deployment guidance for enterprise architects, developers, and IT.",
+    link: "../setup/deploy/other/docker",
+    title: "Docker",
+    image: IconDocker,
+    description: "Use Docker to deploy and run Camunda 8 Self-Managed.",
   },
   {
-    link: "../update",
-    title: "Update",
-    image: IconUpdate,
-    description:
-      "Learn how to upgrade via Helm and update to a newer version.",
+    link: "../setup/deploy/local/manual",
+    title: "Manual",
+    image: IconManual,
+    description: "Manually install Camunda 8 on a local or virtual machine.",
+  },
+];
+
+// Reference architectures cards on introduction page
+export const referenceCards = [
+  {
+    link: "../reference-architecture/kubernetes",
+    title: "Kubernetes",
+    image: IconKubernetes,
+    description: "Deploy Camunda 8 Self-Managed within a Kubernetes cluster.",
+  },
+  {
+    link: "../setup/deploy/other/docker",
+    title: "Manual Jar",
+    image: IconJar,
+    description: "Use Docker to deploy and run Camunda 8 Self-Managed.",
+  },
+  {
+    link: "../setup/deploy/local/manual",
+    title: "Manual",
+    image: IconManual,
+    description: "Manually install Camunda 8 on a local or virtual machine.",
+  },
+    {
+    link: "../reference-architecture/kubernetes",
+    title: "Kubernetes",
+    image: IconKubernetes,
+    description: "Guidance for deploying Camunda 8 Self-Managed within a Kubernetes cluster.",
+  },
+  {
+    link: "../setup/deploy/other/docker",
+    title: "Manual Jar",
+    image: IconJar,
+    description: "Use Docker to deploy and run Camunda 8 Self-Managed.",
+  },
+  {
+    link: "../setup/deploy/local/manual",
+    title: "Manual",
+    image: IconManual,
+    description: "Manually install Camunda 8 on a local or virtual machine.",
   },
 ];
