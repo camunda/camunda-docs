8e4f80f978f0ddfb7f8827e93f43ec1cbdcf28f6
Ahmed AbouZaid <6760103+aabouzaid@users.noreply.github.com>
2025-06-26 21:28:00 +0200

    update CamundaDistributions functions

--- src/components/CamundaDistributions/index.jsx
+++ src/components/CamundaDistributions/index.jsx
@@ -1,4 +1,4 @@
 export { default as C8Run } from "./C8Run";
 export { default as DockerCompose } from "./DockerCompose";
-export { default as HelmChartInstall } from "./Helm";
+export { default as HelmChartInstall, HelmChartValuesFileLocalLink } from "./Helm";
 export { default as PageDescription } from "../PageDescription";
