c2bd31b62a3fbe3cbfab4e8a9c0449566b05bfa0
mesellings <mark.sellings@camunda.com>
2025-07-01 14:52:47 +0100

    TW edits to backup and restore

--- versioned_docs/version-8.7/self-managed/operational-guides/backup-restore/react-components/_card-data.js
+++ versioned_docs/version-8.7/self-managed/operational-guides/backup-restore/react-components/_card-data.js
@@ -8,27 +8,23 @@ import IconRepeat from "../assets/repeat.png";
 export const overviewCards = [
   {
     link: "#prerequisites",
-    title: "Configure Storage",
+    title: "Prerequisites",
     image: IconGear,
     description:
-      "1. [Setup a snapshot repository in the secondary datastore](#prerequisites)\n" +
-      "2. [Configure Camunda component backup storage](#prerequisites)",
+      "Set up a snapshot repository in the secondary datastore and configure component backup storage.",
   },
   {
-    link: "#backup-process",
-    title: "Backup",
+    link: "../backup",
+    title: "Create a backup",
     image: IconCloud,
     description:
-      "1. [Backup WebApps](#backup-of-the-webapps)\n" +
-      "2. [Backup Zeebe Cluster](#backup-of-the-zeebe-cluster)\n",
+      "Create a backup. This involves backing up the WebApps and the Zeebe Cluster.",
   },
   {
-    link: "#restore-process",
-    title: "Restore",
+    link: "../restore",
+    title: "Restore a backup",
     image: IconRepeat,
     description:
-      "1. [Restore of secondary datastore](#restore-of-elasticsearch--opensearch)\n" +
-      "2. [Restore Zeebe Cluster](#restore-the-zeebe-cluster)\n" +
-      "3. [Start and use Camunda 8](#start-all-camunda-8-components)",
+      "Restore a backup. This involves restoring Elasticsearch/OpenSearch and the Zeebe Cluster.",
   },
 ];
