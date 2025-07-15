9fa34314dad1ce6009d691687201d21e082bb4a0
Philipp Fromme <philipp.fromme@camunda.com>
2025-07-15 12:17:06 +0200

    chore(bpmn-js): align good and bad indication icons with existing icons

--- static/js/render-diagrams.js
+++ static/js/render-diagrams.js
@@ -38,7 +38,12 @@ async function renderBpmn(element) {
 
     const icon = document.createElement("span");
 
-    icon.textContent = "👍";
+    icon.classList.add("bpmn-io-example-thumbs-icon");
+
+    icon.innerHTML = `<svg class="guideline-mark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <path fill="green" d="M16,2A14,14,0,1,0,30,16,14,14,0,0,0,16,2ZM14,21.5908l-5-5L10.5906,15,14,18.4092,21.41,11l1.5957,1.5859Z"></path>
+  <polygon fill="white" points="14 21.591 9 16.591 10.591 15 14 18.409 21.41 11 23.005 12.585 14 21.591"></polygon>
+</svg>`;
 
     container.appendChild(icon);
   }
@@ -48,7 +53,11 @@ async function renderBpmn(element) {
 
     const icon = document.createElement("span");
 
-    icon.textContent = "👎";
+    icon.classList.add("bpmn-io-example-thumbs-icon");
+
+    icon.innerHTML = `<svg class="guideline-mark" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M8 1C4.1 1 1 4.1 1 8s3.1 7 7 7 7-3.1 7-7-3.1-7-7-7zm2.7 10.5L8 8.8l-2.7 2.7-.8-.8L7.2 8 4.5 5.3l.8-.8L8 7.2l2.7-2.7.8.8L8.8 8l2.7 2.7-.8.8z" fill="#DA1E28"></path>
+</svg>`;
 
     container.appendChild(icon);
   }
@@ -146,7 +155,12 @@ async function renderDmn(element) {
 
     const icon = document.createElement("span");
 
-    icon.textContent = "👍";
+    icon.classList.add("bpmn-io-example-thumbs-icon");
+
+    icon.innerHTML = `<svg class="guideline-mark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <path fill="green" d="M16,2A14,14,0,1,0,30,16,14,14,0,0,0,16,2ZM14,21.5908l-5-5L10.5906,15,14,18.4092,21.41,11l1.5957,1.5859Z"></path>
+  <polygon fill="white" points="14 21.591 9 16.591 10.591 15 14 18.409 21.41 11 23.005 12.585 14 21.591"></polygon>
+</svg>`;
 
     container.appendChild(icon);
   }
@@ -156,7 +170,11 @@ async function renderDmn(element) {
 
     const icon = document.createElement("span");
 
-    icon.textContent = "👎";
+    icon.classList.add("bpmn-io-example-thumbs-icon");
+
+    icon.innerHTML = `<svg class="guideline-mark" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M8 1C4.1 1 1 4.1 1 8s3.1 7 7 7 7-3.1 7-7-3.1-7-7-7zm2.7 10.5L8 8.8l-2.7 2.7-.8-.8L7.2 8 4.5 5.3l.8-.8L8 7.2l2.7-2.7.8.8L8.8 8l2.7 2.7-.8.8z" fill="#DA1E28"></path>
+</svg>`;
 
     container.appendChild(icon);
   }
