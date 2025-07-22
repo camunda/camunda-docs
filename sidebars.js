82ded540ada2d450f34e21fd506d6a343ec96858
Philipp Ossler <philipp.ossler@gmail.com>
2025-07-22 13:57:22 +0200

    feat: Add migration guide for ZPT to CPT
    Add a new guide to migrate process tests written in ZPT to the new testing library: CPT.

--- sidebars.js
+++ sidebars.js
@@ -963,6 +963,7 @@ module.exports = {
       items: [
         "apis-tools/migration-manuals/migrate-to-camunda-user-tasks",
         "apis-tools/migration-manuals/migrate-to-camunda-api",
+        "apis-tools/migration-manuals/migrate-to-camunda-process-test",
       ],
     },
   ],
