79d52c1d2be13ea975812d6ff100c8adf0b4efab
Pablo Agustin Cabrera <pablo.cabrera@camunda.com>
2025-08-27 17:59:28 +0200

    chore: move save as template section

--- sidebars.js
+++ sidebars.js
@@ -173,7 +173,6 @@ module.exports = {
             "components/modeler/web-modeler/git-sync",
             "components/modeler/web-modeler/import-diagram",
             "components/modeler/web-modeler/fix-problems-in-your-diagram",
-            "components/modeler/web-modeler/save-as-element-templates",
             "components/modeler/web-modeler/run-or-publish-your-process",
             "components/modeler/web-modeler/integrate-web-modeler-in-ci-cd",
             {
@@ -203,6 +202,7 @@ module.exports = {
               "Element templates": [
                 "components/modeler/web-modeler/element-templates/manage-element-templates",
                 "components/modeler/web-modeler/element-templates/using-templates-in-web-modeler",
+                "components/modeler/web-modeler/element-templates/save-as-element-templates",
               ],
             },
             "components/modeler/web-modeler/camunda-marketplace",
