d0a5eed66f4a52fb28f3c8e946cb38c6aacd1e15
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-09-01 12:44:05 +0200

    feat: prevent line-break within property

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -9,6 +9,6 @@ export default function Property({ defaultValue, groupId, property, env }) {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const value = searchParams.get(groupId) || defaultValue;
-  const formattedValue = formats[value];
-  return <code>{formattedValue}</code>;
+  const formattedValue = formats[value]; // non-breaking hyphen
+  return <code style={{ "white-space": "nowrap" }}>{formattedValue}</code>;
 }
