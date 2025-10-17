bfc76dc8f2ac47121594c019d0075ac98c19ed2d
mesellings <mark.sellings@camunda.com>
2025-10-17 16:40:15 +0100

    create a button

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,6 +91,15 @@ const search_feel_url = "/docs/components/modeler/feel/what-is-feel/";
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
+
+  const openKapa = () => {
+    if (typeof window !== "undefined" && window.Kapa?.open) {
+      window.Kapa.open({});
+    } else {
+      console.warn("Kapa widget is not loaded yet.");
+    }
+  };
+
   return (
     <Layout
       title={`${siteConfig.title}`}
@@ -141,6 +150,13 @@ function Home() {
                 Sign up
               </Link>
             </div>
+            <button
+              type="button"
+              className="button button--secondary button--lg"
+              onClick={openKapa}
+            >
+              Open AI assistant
+            </button>
           </div>
         </div>
       </header>
