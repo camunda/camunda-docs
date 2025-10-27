d16c1bab4a08cb1a2618be3976c6eb7cb64eee32
Zainab Daodu <zainab.daodu@camunda.com>
2025-10-27 20:08:34 +0100

    Fix: broken links

--- versioned_docs/version-8.8/self-managed/deployment/helm/configure/authentication-and-authorization/react-components/_card-data.js
+++ versioned_docs/version-8.8/self-managed/deployment/helm/configure/authentication-and-authorization/react-components/_card-data.js
@@ -8,21 +8,21 @@ import IconGear from "../../../../../../components/assets/icon-orchcluster.png";
 // Backup and Restore overview cards data
 export const overviewCards = [
   {
-    link: "../external-keycloak.md",
+    link: "/self-managed/deployment/helm/configure/authentication-and-authorization/external-keycloak.md",
     title: "External Keycloak",
     image: IconGear,
     description:
       "Use an externally hosted Keycloak instance where the Camunda Management Identity component can automatically populate the realm.",
   },
   {
-    link: "../microsoft-entra.md",
+    link: "/self-managed/deployment/helm/configure/authentication-and-authorization/microsoft-entra.md",
     title: "Microsoft Entra",
     image: IconGear,
     description:
       "Connect Camunda to Microsoft Entra ID for authentication and authorization.",
   },
   {
-    link: "../generic-oidc-provider.md",
+    link: "/self-managed/deployment/helm/configure/authentication-and-authorization/generic-oidc-provider.md",
     title: "Generic OIDC Provider",
     image: IconGear,
     description:
