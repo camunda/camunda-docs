ac27e4969176205a46fda382cbc4a01901241b5b
mesellings <mark.sellings@camunda.com>
2026-06-04 09:21:40 +0100

    docs(build): remove alpha5

--- src/pages/build-with-camunda.js
+++ src/pages/build-with-camunda.js
@@ -1039,7 +1039,7 @@ $ c8ctl run rocket-launch.bpmn --variables='{"fuelLevel":90}'`}
             <div className={styles.commandCard}>
               <h4>Manage your clusters</h4>
               <TerminalWindow title="Terminal">
-                {`$ c8ctl cluster start 8.9.0-alpha5
+                {`$ c8ctl cluster start 8.9
 $ c8ctl cluster stop`}
               </TerminalWindow>
             </div>
