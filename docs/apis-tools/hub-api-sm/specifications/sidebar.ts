eb262eff549226f32d8c35c8ef533245f944f0dc
Alex Ronquillo <alex.ronquillo@camunda.com>
2026-06-08 10:43:13 +0200

    docs(hub): pull latest specs

--- docs/apis-tools/hub-api-sm/specifications/sidebar.ts
+++ docs/apis-tools/hub-api-sm/specifications/sidebar.ts
@@ -42,6 +42,72 @@ const sidebar: SidebarsConfig = {
         },
       ],
     },
+    {
+      type: "category",
+      label: "Folder",
+      items: [
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/create-folder",
+          label: "Create a folder",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/get-folder",
+          label: "Get a folder",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/update-folder",
+          label: "Update a folder",
+          className: "api-method patch",
+        },
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/delete-folder",
+          label: "Delete a folder",
+          className: "api-method delete",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Project",
+      items: [
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/create-project",
+          label: "Create a project",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/get-project",
+          label: "Get a project",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/update-project",
+          label: "Update a project",
+          className: "api-method patch",
+        },
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/delete-project",
+          label: "Delete a project",
+          className: "api-method delete",
+        },
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/search-projects",
+          label: "Search projects",
+          className: "api-method post",
+        },
+      ],
+    },
   ],
 };
 
