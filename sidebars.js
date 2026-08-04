d021e28dfc2ea3c479ba55d0533c694b5a6d7bf7
Panagiotis Goutis <panagiotis.goutis@camunda.com>
2026-08-04 10:16:37 +0300

    docs: document cluster mode change behavior

--- sidebars.js
+++ sidebars.js
@@ -2353,6 +2353,7 @@ module.exports = {
                     "self-managed/components/orchestration-cluster/zeebe/operations/management-api",
                     "self-managed/components/orchestration-cluster/zeebe/operations/backups",
                     "self-managed/components/orchestration-cluster/zeebe/operations/cluster-scaling",
+                    "self-managed/components/orchestration-cluster/zeebe/operations/modes",
                   ],
                 },
                 {
