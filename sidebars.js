e238dcc2df8e9c78cf23bff561317eddec9973a3
Alex Ronquillo <alex.ronquillo@camunda.com>
2026-09-09 16:52:11 +0200

    docs(hub): add comment

--- sidebars.js
+++ sidebars.js
@@ -778,6 +778,7 @@ module.exports = {
                 "components/hub/organization/manage-organization-settings/view-organization-activity",
                 "components/hub/organization/manage-organization-settings/enable-alpha-features",
                 "components/hub/organization/manage-organization-settings/usage-history",
+                // TODO: Either bring these back when we have content or remove references
                 // "components/hub/organization/manage-organization-settings/usage-alerts",
                 // "components/hub/organization/manage-organization-settings/advanced-search",
                 "components/hub/organization/manage-organization-settings/switch-organization",
