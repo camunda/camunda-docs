862c39df3ce7390b64784b3c65b566424d6d8da3
Alex Ronquillo <alex.ronquillo@camunda.com>
2026-09-16 09:25:10 +0200

    dx: run prettier

--- src/components/ExpandableTable/index.jsx
+++ src/components/ExpandableTable/index.jsx
@@ -40,9 +40,17 @@ export default function ExpandableTable({ title, children }) {
             className="expand-button"
             onClick={handleShowFullscreen}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+            >
               <path d="M0 0h24v24H0z" fill="none" />
-              <path fill="currentColor" d="m21 15.344l-2.121 2.121l-3.172-3.172l-1.414 1.414l3.172 3.172L15.344 21H21zM3 8.656l2.121-2.121l3.172 3.172l1.414-1.414l-3.172-3.172L8.656 3H3zM21 3h-5.656l2.121 2.121l-3.172 3.172l1.414 1.414l3.172-3.172L21 8.656zM3 21h5.656l-2.121-2.121l3.172-3.172l-1.414-1.414l-3.172 3.172L3 15.344z" />
+              <path
+                fill="currentColor"
+                d="m21 15.344l-2.121 2.121l-3.172-3.172l-1.414 1.414l3.172 3.172L15.344 21H21zM3 8.656l2.121-2.121l3.172 3.172l1.414-1.414l-3.172-3.172L8.656 3H3zM21 3h-5.656l2.121 2.121l-3.172 3.172l1.414 1.414l3.172-3.172L21 8.656zM3 21h5.656l-2.121-2.121l3.172-3.172l-1.414-1.414l-3.172 3.172L3 15.344z"
+              />
             </svg>
             Show full table
           </button>
@@ -63,7 +71,9 @@ export default function ExpandableTable({ title, children }) {
           />
           <div className="expanded-table-modal-contents">
             <div className="expanded-table-modal-header">
-              {title ? <h2 className="expanded-table-modal-title">{title}</h2> : null}
+              {title ? (
+                <h2 className="expanded-table-modal-title">{title}</h2>
+              ) : null}
 
               <button
                 ref={closeButtonRef}
@@ -71,9 +81,17 @@ export default function ExpandableTable({ title, children }) {
                 aria-label="Close"
                 onClick={handleClose}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 32 32"
+                >
                   <path d="M0 0h32v32H0z" fill="none" />
-                  <path fill="currentColor" d="M17.414 16L24 9.414L22.586 8L16 14.586L9.414 8L8 9.414L14.586 16L8 22.586L9.414 24L16 17.414L22.586 24L24 22.586z" />
+                  <path
+                    fill="currentColor"
+                    d="M17.414 16L24 9.414L22.586 8L16 14.586L9.414 8L8 9.414L14.586 16L8 22.586L9.414 24L16 17.414L22.586 24L24 22.586z"
+                  />
                 </svg>
               </button>
             </div>
