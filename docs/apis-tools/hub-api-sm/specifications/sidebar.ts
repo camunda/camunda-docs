2b9e2903c2a90903eaad2341902771d13fd10f21
alexronquillo <8672090+alexronquillo@users.noreply.github.com>
2026-09-18 15:19:04 +0000

    docs: update Camunda Hub REST API spec

--- docs/apis-tools/hub-api-sm/specifications/sidebar.ts
+++ docs/apis-tools/hub-api-sm/specifications/sidebar.ts
@@ -34,6 +34,12 @@ const sidebar: SidebarsConfig = {
           label: "Search the projects using a catalog asset",
           className: "api-method post",
         },
+        {
+          type: "doc",
+          id: "apis-tools/hub-api-sm/specifications/search-catalog-asset-file-usages",
+          label: "Search the files using a catalog asset",
+          className: "api-method post",
+        },
       ],
     },
     {
