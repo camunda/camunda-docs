73145c26e206efb78dac1608598137bf4e7f4d78
Alex Ronquillo <alex.ronquillo@camunda.com>
2026-09-21 08:31:59 +0200

    dx: fix builds

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ module.exports = {
     v4: {
       removeLegacyPostBuildHeadAttribute: true,
     },
-    experimental_faster: true,
+    faster: true,
   },
   title: "Camunda 8 Docs",
   tagline:
